abe3b0664f8ebb35a389596987fdd232d5af6419
anggardhanoano <anggardhanoano@gmail.com>
2022-03-01 23:25:26 +0700

    knlkj

--- src/commons/apolloClient.ts
+++ src/commons/apolloClient.ts
@@ -9,7 +9,9 @@ import { setContext } from '@apollo/client/link/context';
 import { toast } from 'react-toastify';
 
 const httpLink = createHttpLink({
-    uri: `${process.env.API_ENDPOINT}/graphql`
+    uri: `${
+        process.env.API_ENDPOINT || process.env.NEXT_PUBLIC_API_ENDPOINT
+    }/graphql`
 });
 
 const authLink = setContext((_, { headers }) => {
