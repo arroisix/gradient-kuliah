333bae2f99eaf7d1d493d3b8b72f59e40500de14
Anggardha Febriano <44425096+anggardhanoano@users.noreply.github.com>
2022-09-17 09:47:38 +0700

    pixe

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,6 +33,9 @@ export default class MyDocument extends Document {
                         integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
                         crossOrigin="anonymous"
                     />
+                    <noscrript>
+                        <img style={{display: "hidden"}} src="https://facebook.com/tr?4d=400978621960262&ev=PageView&noscript=1" width="1" height="1"
+                    /></noscrript>
                 </Head>
                 <body>
                     <Main />
