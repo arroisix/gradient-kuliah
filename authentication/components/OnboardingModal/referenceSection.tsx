a7f28fa35b72c7dea8ff6225749c7e2fdaf1b196
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2022-11-01 01:19:37 +0700

    chore: remove debug code

--- authentication/components/OnboardingModal/referenceSection.tsx
+++ authentication/components/OnboardingModal/referenceSection.tsx
@@ -4,7 +4,6 @@ import Radio from 'commons/components/elements/Form/radio';
 import { useGetRegisterReferenceQuery } from 'authentication/redux/api/authApi';
 import { useContext } from 'react';
 import RegistrationContext from 'authentication/contexts/RegistrationProvider';
-import { useEffect } from 'react';
 
 const ReferenceSection = (): JSX.Element => {
     const { data: registerReferences, isLoading: isLoadingReferences } =
@@ -13,30 +12,27 @@ const ReferenceSection = (): JSX.Element => {
 
     return (
         <div className="w-full flex flex-col">
-            <Formik
-                initialValues={{} as UpdateUserInputData}
-                onSubmit={async (values, { setSubmitting }) => {
-                    setSubmitting(true);
-                    await updateUser({ ...formData, ...values });
-                    setSubmitting(false);
-                }}>
-                {({
-                    values,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-                }) => {
-                    useEffect(() => {
-                        if (!isLoadingReferences && registerReferences) {
-                            values.register_reference_id =
-                                registerReferences.data[0].id;
-                        }
-                    }, [registerReferences, isLoadingReferences]);
-
-                    return (
-                        <form onSubmit={handleSubmit} className="container">
-                            {!isLoadingReferences && registerReferences ? (
+            {!isLoadingReferences && registerReferences ? (
+                <Formik
+                    initialValues={
+                        {
+                            register_reference_id: registerReferences.data[0].id
+                        } as UpdateUserInputData
+                    }
+                    onSubmit={async (values, { setSubmitting }) => {
+                        setSubmitting(true);
+                        await updateUser({ ...formData, ...values });
+                        setSubmitting(false);
+                    }}>
+                    {({
+                        values,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting
+                    }) => {
+                        return (
+                            <form onSubmit={handleSubmit} className="container">
                                 <Radio
                                     onChange={handleChange}
                                     onBlur={handleBlur}
@@ -50,29 +46,31 @@ const ReferenceSection = (): JSX.Element => {
                                         })
                                     )}
                                 />
-                            ) : null}
-                            <div className="flex space-x-2">
-                                <Button
-                                    variant="custom"
-                                    className="bg-neutral-100 text-neutral-700 mt-4 w-full"
-                                    type="button"
-                                    onClick={() => {
-                                        setStep(0);
-                                    }}>
-                                    Kembali
-                                </Button>
-                                <Button
-                                    variant="custom"
-                                    className="bg-accent-purple text-white mt-4 w-full"
-                                    type="submit"
-                                    disabled={isSubmitting}>
-                                    {isSubmitting ? 'Menyimpan...' : 'Simpan'}
-                                </Button>
-                            </div>
-                        </form>
-                    );
-                }}
-            </Formik>
+                                <div className="flex space-x-2">
+                                    <Button
+                                        variant="custom"
+                                        className="bg-neutral-100 text-neutral-700 mt-4 w-full"
+                                        type="button"
+                                        onClick={() => {
+                                            setStep(0);
+                                        }}>
+                                        Kembali
+                                    </Button>
+                                    <Button
+                                        variant="custom"
+                                        className="bg-accent-purple text-white mt-4 w-full"
+                                        type="submit"
+                                        disabled={isSubmitting}>
+                                        {isSubmitting
+                                            ? 'Menyimpan...'
+                                            : 'Simpan'}
+                                    </Button>
+                                </div>
+                            </form>
+                        );
+                    }}
+                </Formik>
+            ) : null}
         </div>
     );
 };
