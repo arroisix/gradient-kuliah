34b1ee0f92c2ee0c2155ce87c18fbb33e4bb061c
anggardhanoano <anggardhanoano@gmail.com>
2022-12-20 22:53:59 +0700

    done

--- courses/components/LandingPage/PTSL/HeroPTSLSection.tsx
+++ courses/components/LandingPage/PTSL/HeroPTSLSection.tsx
@@ -57,11 +57,11 @@ const HeroPTSLSection = ({
         <section className="h-screen w-full flex flex-col-reverse md:flex-row justify-end md:justify-center relative">
             <div className="w-full h-screen flex flex-col justify-end md:justify-center pl-4 pr-4 md:pr-0 md:pl-[7.5rem] py-4 mb-8 md:mb-0">
                 {!is_subscribed ? (
-                    <div className="z-10">
+                    <div className="z-10 w-full lg:w-[40vw]">
                         <h1 className="text-4xl md:text-5xl font-bold">
                             {title}
                         </h1>
-                        <div className="w-full md:w-1/3 my-4">
+                        <div className="w-full my-4">
                             <p>{tagline}</p>
                         </div>
                     </div>
