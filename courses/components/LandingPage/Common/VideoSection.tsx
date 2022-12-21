cdccc82f016d2ca5e5a579bcc40bb755c3b48555
anggardhanoano <anggardhanoano@gmail.com>
2022-12-21 18:52:59 +0700

    done fix responsiveness

--- courses/components/LandingPage/Common/VideoSection.tsx
+++ courses/components/LandingPage/Common/VideoSection.tsx
@@ -56,7 +56,7 @@ const VideoSection = ({ slug }: VideoSectionProps): JSX.Element => {
                 Coba Gratis Video Belajar
             </h1>
             <div
-                className={`flex flex-col lg:flex-row lg:max-h-[600px] ${
+                className={`flex flex-col lg:flex-row lg:h-[425px] 2xl:h-[600px] ${
                     !isVideoContentExist && 'justify-center'
                 }`}>
                 {isVideoContentExist && (
@@ -65,7 +65,13 @@ const VideoSection = ({ slug }: VideoSectionProps): JSX.Element => {
                         id="video-section">
                         {videoPicked && videoPicked?.is_free ? (
                             <VideoPlayer
-                                height={width >= 1024 ? '600px' : ''}
+                                height={
+                                    width >= 1536
+                                        ? '600px'
+                                        : width >= 1024
+                                        ? '425px'
+                                        : ''
+                                }
                                 video={videoPicked.video_url as string}
                                 thumbnail={videoPicked.thumbnail as string}
                                 key={videoPicked.video_url as string}
