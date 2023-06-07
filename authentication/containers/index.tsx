34a63051034cf03c032e7705f03f399de1dab702
DJaegerScript <adjiedjaka0902@gmail.com>
2023-06-07 12:53:54 +0700

    created header for onboarding page

--- authentication/containers/index.tsx
+++ authentication/containers/index.tsx
@@ -2,15 +2,20 @@ import { useGoogleLogin } from '@react-oauth/google';
 import { AUTH_SECTION } from 'authentication/constants';
 import { RegistrationProvider } from 'authentication/contexts/RegistrationProvider';
 import useSocialLogin from 'authentication/hooks/useSocialLogin';
+import { getCurrentUser } from 'authentication/redux/selectors/userSelector';
+import { removeUser } from 'authentication/redux/slices/userSlice';
 import Button from 'commons/components/elements/Button';
 import { useRouter } from 'next/router';
 import { AiOutlineGoogle } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 
 export const AuthenticationContainer: React.FC = () => {
     const { pathname } = useRouter();
     const AuthSection = AUTH_SECTION[pathname];
     const { googleLogin } = useSocialLogin();
+    const user = useSelector(getCurrentUser);
+    const dispatch = useDispatch();
 
     const login = useGoogleLogin({
         onSuccess: async (tokenResponse) => {
@@ -21,8 +26,22 @@ export const AuthenticationContainer: React.FC = () => {
         }
     });
 
+    const logout: () => void = () => dispatch(removeUser());
+
     return (
         <section className="flex items-center justify-center w-screen min-h-screen text-white bg-neutral-1000">
+            {pathname === '/onboarding' && (
+                <div className="fixed top-0 left-0 flex gap-2 px-6 py-3 font-body">
+                    <span className="text-[#666666]">
+                        Terdaftar sebagai {user.email}
+                    </span>{' '}
+                    <button
+                        onClick={logout}
+                        className="text-[#999999] hover:text-red-400 transition-all duration-500">
+                        Sign Out
+                    </button>
+                </div>
+            )}
             <div
                 className={`${
                     pathname !== '/onboarding' ? 'w-[324px]' : 'w-[360px]'
