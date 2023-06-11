8ddfd378af77c8f6d8157343a631e1ebbce44cd9
DJaegerScript <adjiedjaka0902@gmail.com>
2023-06-11 15:42:36 +0700

    use explicit conditional statement instead of ternary operator

--- profile/components/EditProfile.tsx
+++ profile/components/EditProfile.tsx
@@ -7,20 +7,22 @@ import { FaAngleLeft } from 'react-icons/fa';
 export const EditProfile = (): JSX.Element => {
     const { profile, menuName } = useProfileContext();
 
-    return !profile ? (
-        <LoadingBackdrop />
-    ) : (
-        <div className="flex flex-col w-full gap-4">
-            <div className="flex items-center w-full gap-4">
-                <Link href={'/profil'}>
-                    <FaAngleLeft
-                        size={24}
-                        className="hover:text-[#7264EB] transition-all duration-500 cursor-pointer"
-                    />
-                </Link>
-                <span className="text-lg font-semibold">{menuName}</span>
+    if (!profile) {
+        return <LoadingBackdrop />;
+    } else {
+        return (
+            <div className="flex flex-col w-full gap-4">
+                <div className="flex items-center w-full gap-4">
+                    <Link href={'/profil'}>
+                        <FaAngleLeft
+                            size={24}
+                            className="hover:text-[#7264EB] transition-all duration-500 cursor-pointer"
+                        />
+                    </Link>
+                    <span className="text-lg font-semibold">{menuName}</span>
+                </div>
+                <PersonalDataForm {...profile} />
             </div>
-            <PersonalDataForm {...profile} />
-        </div>
-    );
+        );
+    }
 };
