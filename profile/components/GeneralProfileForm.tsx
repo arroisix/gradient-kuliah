9f4ffe6da0b37ba404f6be7261f4c04b4f6ec346
DJaegerScript <adjiedjaka0902@gmail.com>
2023-06-12 01:33:02 +0700

    created username validation with useDebounce

--- profile/components/GeneralProfileForm.tsx
+++ profile/components/GeneralProfileForm.tsx
@@ -2,16 +2,42 @@ import { useCheckUsernameAvailabilityMutation } from 'authentication/redux/api/a
 import Button from 'commons/components/elements/Button';
 import Input from 'commons/components/elements/Form/input';
 import Spinner from 'commons/components/elements/Spinner';
+import { useDebouncedCallback } from 'use-debounce';
 import { Formik } from 'formik';
 import {
     ReduxHTTPError,
     useProfileContext
 } from 'profile/contexts/ProfileProvider';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { FaCheckCircle, FaSpinner, FaTimesCircle } from 'react-icons/fa';
 
 export const GeneralProfileForm = (): JSX.Element => {
     const { isLoading, updateUser, profile } = useProfileContext();
-    const [checkUsernameAvailability] = useCheckUsernameAvailabilityMutation();
+    const [checkUsernameAvailability, { isLoading: isCheckUsernameLoading }] =
+        useCheckUsernameAvailabilityMutation();
+    const [isValid, setIsValid] = useState(false);
+
+    useEffect(() => {
+        console.log(isValid);
+    }, [isValid]);
+
+    const debounced = useDebouncedCallback(async (value) => {
+        const result = await checkUsernameAvailability({
+            username: value
+        });
+
+        const {
+            data: { is_available }
+        } = result as CheckUsernameAvailabilityResponse;
+        setIsValid(is_available);
+    }, 1000);
+
+    const Icon = isValid
+        ? FaCheckCircle
+        : isCheckUsernameLoading
+        ? FaSpinner
+        : FaTimesCircle;
 
     return (
         <Formik
@@ -41,21 +67,9 @@ export const GeneralProfileForm = (): JSX.Element => {
             validate={async (values) => {
                 const errors: { [key: string]: string } = {};
 
-                if (!values.full_name) errors.full_name = 'Required';
-                if (!values.username) errors.username = 'Required';
-
-                if (!!values.username) {
-                    const result = await checkUsernameAvailability({
-                        username: values.username
-                    });
-
-                    const {
-                        data: { is_available }
-                    } = result as CheckUsernameAvailabilityResponse;
-
-                    if (!is_available)
-                        errors.username = 'Username is not available';
-                }
+                if (!values.full_name)
+                    errors.full_name = 'Nama lengkap dibutuhkan';
+                if (!values.username) errors.username = 'Username dibutuhkan';
 
                 return errors;
             }}
@@ -91,18 +105,51 @@ export const GeneralProfileForm = (): JSX.Element => {
                             type="text"
                             placeholder="Username"
                             name="username"
-                            onChange={handleChange}
+                            endAddorment={
+                                initialValues !== values ? (
+                                    <Icon
+                                        className={` ${
+                                            isValid
+                                                ? 'text-green-600'
+                                                : isLoading
+                                                ? 'animate-spin text-[#999999]'
+                                                : 'text-red-600'
+                                        }`}
+                                    />
+                                ) : undefined
+                            }
+                            className={
+                                initialValues === values
+                                    ? ''
+                                    : isValid
+                                    ? 'border-green-600'
+                                    : 'border-red-600'
+                            }
+                            onChange={async (event) => {
+                                handleChange(event);
+                                setIsValid(false);
+                                if (event.target.value !== '') {
+                                    await debounced(event.target.value);
+                                }
+                            }}
                             onBlur={handleBlur}
                             value={values.username}
                             error={
                                 touched.username && errors.username
                                     ? errors.username
+                                    : !isValid
+                                    ? 'Username tidak tersedia'
                                     : undefined
                             }
                         />
                     </div>
                     <Button
-                        disabled={isLoading || initialValues === values}
+                        disabled={
+                            isLoading ||
+                            initialValues === values ||
+                            isCheckUsernameLoading ||
+                            !isValid
+                        }
                         variant="custom"
                         className="w-full mt-4 text-white bg-accent-purple"
                         type="submit">
