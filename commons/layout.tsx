f5de63851bd3d1ea4736953ee69580631da1b8f2
IrPanTech <irfannkamill@gmail.com>
2023-06-13 11:05:17 +0700

    improve layout by adding sidebar

--- commons/layout.tsx
+++ commons/layout.tsx
@@ -1,19 +1,28 @@
+import { useSelector } from 'react-redux';
 import Footer from './components/modules/Footer';
 import Navbar from './components/modules/Navbar';
+import Sidebar from './components/modules/Sidebar';
+import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
 
 interface LayoutProps {
     children?: JSX.Element;
     paymentPage?: boolean;
     shouldTransparent?: boolean;
     courses?: Course[];
+    showSidebar?: boolean;
+    fullHeightSidebar?: boolean;
 }
 
 const Layout = ({
     children,
     paymentPage,
     shouldTransparent,
-    courses
+    courses,
+    showSidebar,
+    fullHeightSidebar
 }: LayoutProps): JSX.Element => {
+    const isAuthenticated = useSelector(getIsAuthenticated);
+
     return (
         <div className="relative w-screen min-h-screen text-white bg-neutral-1000">
             <Navbar
@@ -21,7 +30,17 @@ const Layout = ({
                 shouldTransparent={shouldTransparent ?? false}
                 courses={courses}
             />
-            <div className="min-h-full">{children}</div>
+            <section
+                className={`min-h-screen ${
+                    showSidebar && isAuthenticated
+                        ? 'pt-24 pb-10 px-4 md:pl-5 md:pr-[5rem] lg:pr-[7.5rem] flex gap-[3rem] lg:gap-[10rem]'
+                        : ''
+                }`}>
+                {showSidebar && isAuthenticated && (
+                    <Sidebar fullHeight={fullHeightSidebar} />
+                )}
+                <div className="min-h-full w-full">{children}</div>
+            </section>
             <Footer />
         </div>
     );
--- commons/learnLayout.tsx
+++ commons/learnLayout.tsx
@@ -1,4 +1,7 @@
+import { useSelector } from 'react-redux';
 import Navbar from './components/modules/Navbar';
+import Sidebar from './components/modules/Sidebar';
+import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
 
 interface LayoutProps {
     children: JSX.Element;
@@ -6,6 +9,8 @@ interface LayoutProps {
     shouldTransparent?: boolean;
     courses?: Course[];
     hideNavbar?: boolean;
+    showSidebar?: boolean;
+    fullHeightSidebar?: boolean;
     lightMode?: boolean;
 }
 
@@ -15,8 +20,12 @@ const LearnLayout = ({
     shouldTransparent,
     courses,
     hideNavbar,
+    showSidebar,
+    fullHeightSidebar,
     lightMode
 }: LayoutProps): JSX.Element => {
+    const isAuthenticated = useSelector(getIsAuthenticated);
+
     return (
         <div className="relative w-screen min-h-screen text-white bg-neutral-1000">
             {!hideNavbar && (
@@ -27,7 +36,19 @@ const LearnLayout = ({
                     courses={courses}
                 />
             )}
-            <div className="min-h-full md:h-[100vh - 65px]">{children}</div>
+            <section
+                className={`min-h-screen ${
+                    showSidebar && isAuthenticated
+                        ? 'pt-24 pb-10 px-4 md:pl-5 md:pr-[5rem] lg:pr-[7.5rem] flex gap-[3rem] lg:gap-[10rem]'
+                        : ''
+                }`}>
+                {showSidebar && isAuthenticated && (
+                    <Sidebar fullHeight={fullHeightSidebar} />
+                )}
+                <div className="min-h-full md:h-[100vh - 65px] w-full">
+                    {children}
+                </div>
+            </section>
         </div>
     );
 };
