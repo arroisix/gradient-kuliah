6f25c956012b05f1c769e8b7423f3faffbd4bfc7
IrPanTech <irfannkamill@gmail.com>
2023-06-14 23:09:52 +0700

    develop kelasku component

--- dashboard/components/MyClass.tsx
+++ dashboard/components/MyClass.tsx
@@ -1,7 +1,6 @@
 import LoadingBackdrop from 'commons/components/elements/LoadingBackdrop';
 import useTransition from 'commons/hooks/useTransition';
 import { useRouter } from 'next/router';
-import { AiOutlinePlus } from 'react-icons/ai';
 
 const dummyData = {
     courses: [
@@ -24,25 +23,31 @@ const dummyData = {
 // };
 
 const MyClass = ({ className }: { className?: string }): JSX.Element => {
+    const router = useRouter();
+    const loadingTransition = useTransition(router);
+
     return (
         <div className={`flex flex-col gap-3 md:gap-5 ${className}`}>
             <h4 className="text-lg font-extrabold">Kelasku</h4>
             <div>
                 {dummyData?.courses.length === 0 ? (
-                    <div className="w-full bg-[#121212] p-2 rounded-[20px]">
-                        <AiOutlinePlus className="mx-auto text-[#373737]" />
+                    <div
+                        className="w-full bg-accent-purple p-2 rounded-[20px] text-center font-extrabold"
+                        onClick={() => router.push('/langganan')}
+                        aria-hidden>
+                        <span>Akses Kelas</span>
                     </div>
                 ) : (
                     <ListMyClass />
                 )}
             </div>
+            {loadingTransition && <LoadingBackdrop />}
         </div>
     );
 };
 
 const ListMyClass = (): JSX.Element => {
     const router = useRouter();
-    const loadingTransition = useTransition(router);
 
     return (
         <div className="flex flex-col gap-3">
@@ -52,10 +57,9 @@ const ListMyClass = (): JSX.Element => {
                     className="w-full bg-[#121212] hover:bg-accent-purple py-2 px-4 rounded-[20px] cursor-pointer transition-all"
                     onClick={() => router.push(`/kelas/${course_slug}`)}
                     aria-hidden={true}>
-                    <span>{course_name}</span>
+                    <span className="font-body text-sm">{course_name}</span>
                 </div>
             ))}
-            {loadingTransition && <LoadingBackdrop />}
         </div>
     );
 };
