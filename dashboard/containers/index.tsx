8e90556a64b4334d3d985f4938dc2e15c50ccacd
IrPanTech <irfannkamill@gmail.com>
2023-06-14 23:10:14 +0700

    develop dashboard page

--- dashboard/containers/index.tsx
+++ dashboard/containers/index.tsx
@@ -1,22 +1,11 @@
-import { removeUser } from 'authentication/redux/slices/userSlice';
-import Button from 'commons/components/elements/Button';
 import ContinueLearning from 'dashboard/components/ContinueLearning';
 import MyClass from 'dashboard/components/MyClass';
-import { useDispatch } from 'react-redux';
+import OfferNotification from 'dashboard/components/OfferNotification';
 
 const DashboardContainer = (): JSX.Element => {
-    const dispatch = useDispatch();
-
-    const logout = (): void => {
-        dispatch(removeUser());
-    };
-
     return (
-        <section className="min-h-screen pt-24 px-4 md:px-[7.5rem]">
-            <h1>HALO</h1>
-            <Button variant="primary" onClick={logout}>
-                Logout
-            </Button>
+        <section className="min-h-screen flex flex-col gap-6">
+            <OfferNotification />
             <div className="flex flex-col md:flex-row-reverse gap-[2rem]">
                 <MyClass className="w-full md:w-3/12" />
                 <ContinueLearning className="w-full md:w-9/12" />
