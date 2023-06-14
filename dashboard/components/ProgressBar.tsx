86a3897530cd8cd2031d7588f9c2f17173bfde32
IrPanTech <irfannkamill@gmail.com>
2023-06-14 23:09:41 +0700

    develop progress bar component

---
+++ dashboard/components/ProgressBar.tsx
@@ -0,0 +1,18 @@
+const ProgressBar = ({ percent = '0%' }: { percent?: string }): JSX.Element => {
+    return (
+        <div>
+            <div className="relative w-[180px] h-[10px]">
+                <div className="absolute w-full h-full bg-[#D9D9D9] rounded-[10px]"></div>
+                <div
+                    className="absolute h-full bg-[#D9B8FF] rounded-[10px]"
+                    style={{ width: percent }}></div>
+            </div>
+            <span className="text-xs">
+                Progress Belajar:{' '}
+                <span className="text-accent-green">{percent}</span>
+            </span>
+        </div>
+    );
+};
+
+export default ProgressBar;
