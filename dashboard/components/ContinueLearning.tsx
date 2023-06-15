6826cbc4c4b7abb7fbb74909469d6eb54ab1632b
IrPanTech <irfannkamill@gmail.com>
2023-06-15 20:33:53 +0700

    create lanjut belajar empty state component

--- dashboard/components/ContinueLearning.tsx
+++ dashboard/components/ContinueLearning.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { HiOutlinePlusSm } from 'react-icons/hi';
 import ProgressBar from './ProgressBar';
 import useCourseSubscription from 'courses/hooks/useCourseSubscription';
 import { useGetStudentLearningProgressQuery } from 'dashboard/redux/api/dashboardApi';
+import Button from 'commons/components/elements/Button';
 
 const ContinueLearning = ({
     className
@@ -93,20 +93,40 @@ const NoLearningProgress = (): JSX.Element => {
     const { is_subscribed } = useCourseSubscription();
 
     return (
-        <div
-            className="flex flex-col md:flex-row gap-[8px] md:gap-[30px] items-start justify-start md:items-center md:justify-center cursor-pointer"
-            onClick={() =>
-                router.push(`${is_subscribed ? '/kelas' : '/langganan'}`)
-            }
-            aria-hidden>
-            <div className="relative flex items-center justify-center w-[260px] md:w-[160px] lg:w-[260px] min-h-[150px] rounded-lg bg-[#242424]">
-                <HiOutlinePlusSm size={24} className="text-[#373737]" />
-            </div>
-            <div className="flex flex-col gap-[5px] w-full">
-                <span className="inline-block font-body">
-                    Belum ada Progress Belajar
+        <div className="flex flex-col gap-[64px] relative w-full md:max-w-[633px] bg-[#1D1D1D] p-10 rounded-xl overflow-hidden">
+            <div>
+                <h6 className="font-extrabold text-[24px] pb-[10px]">
+                    Belum ada progress belajar
+                </h6>
+                <span className="inline-block font-body sm:max-w-[70%] md:max-w-full lg:max-w-[70%]">
+                    {is_subscribed
+                        ? 'Anda sudah memiliki kelas. Mulai belajar sekarang'
+                        : 'Anda belum memiliki kelas apapun. Mulai dengan  memilih kelas yang tepat umtuk anda'}
                 </span>
-                <ProgressBar percent={'0%'} />
+            </div>
+            <div>
+                <Button
+                    variant="custom"
+                    className="bg-white font-sans font-bold text-black text-xs"
+                    onClick={() =>
+                        router.push(
+                            `${is_subscribed ? '/kelas' : '/langganan'}`
+                        )
+                    }>
+                    {is_subscribed ? 'Mulai Belajar' : 'Beli Kelas'}
+                </Button>
+            </div>
+            <div className="absolute w-[252px] h-[230px] bottom-0 right-0">
+                <Image
+                    src={
+                        is_subscribed
+                            ? 'https://assets.gradient.academy/assets/globe.png'
+                            : 'https://assets.gradient.academy/assets/book-shelf.png'
+                    }
+                    alt={'subchapter_name'}
+                    layout="fill"
+                    className="object-contain"
+                />
             </div>
         </div>
     );
