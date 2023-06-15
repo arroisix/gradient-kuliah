9191e35bdb36dcdc86c546f9d0ade8d0a709b4ab
IrPanTech <irfannkamill@gmail.com>
2023-06-15 11:30:58 +0700

    rendered using real data

--- dashboard/components/MyClass.tsx
+++ dashboard/components/MyClass.tsx
@@ -1,44 +1,40 @@
 import LoadingBackdrop from 'commons/components/elements/LoadingBackdrop';
 import useTransition from 'commons/hooks/useTransition';
+import {
+    StudentCourse,
+    useGetStudentCourseQuery
+} from 'dashboard/redux/api/dashboardApi';
 import { useRouter } from 'next/router';
-
-const dummyData = {
-    courses: [
-        {
-            course_slug: 'fisdas',
-            course_name: 'Fisika Dasar'
-        },
-        {
-            course_slug: 'kimdas1',
-            course_name: 'Kimia Dasar 1'
-        },
-        {
-            course_slug: 'kalkulus2',
-            course_name: 'Kalkulus 2'
-        }
-    ]
-};
-// const dummyData = {
-//     courses: []
-// };
 
 const MyClass = ({ className }: { className?: string }): JSX.Element => {
     const router = useRouter();
     const loadingTransition = useTransition(router);
 
+    const { data, isLoading } = useGetStudentCourseQuery();
+
     return (
         <div className={`flex flex-col gap-3 md:gap-5 ${className}`}>
             <h4 className="text-lg font-extrabold">Kelasku</h4>
             <div>
-                {dummyData?.courses.length === 0 ? (
-                    <div
-                        className="w-full bg-accent-purple p-2 rounded-[20px] text-center font-extrabold"
-                        onClick={() => router.push('/langganan')}
-                        aria-hidden>
-                        <span>Akses Kelas</span>
+                {isLoading ? (
+                    <div className="flex flex-col gap-3">
+                        <div className="w-full h-8 py-2 px-4 rounded-[20px] bg-neutral-800 text-neutral-800 animate-pulse"></div>
+                        <div className="w-full h-8 py-2 px-4 rounded-[20px] bg-neutral-800 text-neutral-800 animate-pulse"></div>
+                        <div className="w-full h-8 py-2 px-4 rounded-[20px] bg-neutral-800 text-neutral-800 animate-pulse"></div>
                     </div>
                 ) : (
-                    <ListMyClass />
+                    <>
+                        {data?.courses.length === 0 ? (
+                            <div
+                                className="w-full bg-accent-purple p-2 rounded-[20px] text-center font-extrabold"
+                                onClick={() => router.push('/langganan')}
+                                aria-hidden>
+                                <span>Akses Kelas</span>
+                            </div>
+                        ) : (
+                            <ListMyClass courses={data?.courses} />
+                        )}
+                    </>
                 )}
             </div>
             {loadingTransition && <LoadingBackdrop />}
@@ -46,12 +42,16 @@ const MyClass = ({ className }: { className?: string }): JSX.Element => {
     );
 };
 
-const ListMyClass = (): JSX.Element => {
+const ListMyClass = ({
+    courses
+}: {
+    courses?: StudentCourse[];
+}): JSX.Element => {
     const router = useRouter();
 
     return (
         <div className="flex flex-col gap-3">
-            {dummyData?.courses.map(({ course_slug, course_name }) => (
+            {courses?.map(({ course_slug, course_name }) => (
                 <div
                     key={course_slug}
                     className="w-full bg-[#121212] hover:bg-accent-purple py-2 px-4 rounded-[20px] cursor-pointer transition-all"
