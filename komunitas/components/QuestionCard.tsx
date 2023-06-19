d357235b7cb4e34d366c7cdac3e38cbd29aae2f5
IrPanTech <irfannkamill@gmail.com>
2023-06-19 11:53:50 +0700

    develop question card

--- komunitas/components/QuestionCard.tsx
+++ komunitas/components/QuestionCard.tsx
@@ -1,14 +1,57 @@
+import { useGetProfileQuery } from 'authentication/redux/api/authApi';
+import Image from 'next/image';
+import Avatar from 'react-avatar';
+import { AiOutlineEye } from 'react-icons/ai';
+import { FaCircle, FaRegComment } from 'react-icons/fa';
+
 const QuestionCard = (): JSX.Element => {
+    const { data } = useGetProfileQuery({});
+
     return (
-        <div className="w-full border-[1px] border-neutral-800 rounded-xl">
-            <div>profile</div>
-            <div>pertanayan</div>
-            <div>
-                <div>
-                    <div>mata</div>
-                    <div>komen</div>
+        <div className="w-full border-[1px] border-neutral-800 rounded-xl p-5">
+            <div className="relative flex items-center gap-3">
+                <div className="relative w-[24px] h-[24px]">
+                    {data?.photo_profile ? (
+                        <Image
+                            src={data?.photo_profile}
+                            alt={data?.full_name}
+                            layout="fill"
+                            className="rounded-full object-contain"
+                        />
+                    ) : (
+                        <Avatar name={data?.full_name} size="24" round />
+                    )}
                 </div>
-                <button className="bg-accent-purple">Jawab</button>
+                <div className="flex flex-col md:flex-row md:gap-[6px] md:items-center">
+                    <span className="inline-block font-extrabold text-xs">
+                        {data?.full_name}
+                    </span>
+                    <FaCircle
+                        className="hidden md:block text-neutral-600"
+                        size={4}
+                    />
+                    <span className="inline-block font-body text-xs text-neutral-600">
+                        2h
+                    </span>
+                </div>
+            </div>
+            <div className="text-xs pt-[12px] pb-[18px] lg:pl-[56px]">
+                pertanayan
+            </div>
+            <div className="flex justify-between">
+                <div className="flex gap-6 items-center">
+                    <div className="flex items-center gap-2">
+                        <AiOutlineEye size={18} />
+                        <span className="font-body text-xs">2</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <FaRegComment className="scale-x-[-1]" size={18} />
+                        <span className="font-body text-xs">2</span>
+                    </div>
+                </div>
+                <button className="bg-neutral-800 px-[27px] py-[7.5px] rounded-[70px] font-extrabold text-xs">
+                    Jawab
+                </button>
             </div>
         </div>
     );
