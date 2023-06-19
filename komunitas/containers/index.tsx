321603ecf7d5c0d5a086a22fa1a10bf35fe748dc
IrPanTech <irfannkamill@gmail.com>
2023-06-19 11:53:29 +0700

    improve komunitas layout

--- komunitas/containers/index.tsx
+++ komunitas/containers/index.tsx
@@ -1,10 +1,13 @@
+import QuestionCard from 'komunitas/components/QuestionCard';
 import React from 'react';
 
 const KomunitasContainer = (): JSX.Element => {
     return (
-        <section className="md:flex md:gap-8">
-            <div></div>
-            <div>Pertanyaanku</div>
+        <section className="flex flex-col md:flex-row gap-[2rem]">
+            <div className="w-full md:w-9/12">
+                <QuestionCard />
+            </div>
+            <div className="hidden md:block w-full md:w-3/12">Pertanyaanku</div>
         </section>
     );
 };
