bcd432e09ab63e08fbf1eeb933545ac55863ed3e
IrPanTech <irfannkamill@gmail.com>
2023-06-20 20:54:10 +0700

    create tabs for mobile breakpoint

---
+++ komunitas/components/MobileTabs.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const MobileTabs = (): JSX.Element => {
+    const { pathname } = useRouter();
+
+    return (
+        <div className="relative flex justify-between w-full overflow-hidden">
+            <Link href={'/komunitas'}>
+                <span
+                    className={`text-center text-sm w-1/2 py-[6px] border-b-2 ${
+                        pathname === '/komunitas'
+                            ? 'border-accent-purple font-bold'
+                            : 'border-[#2D2D2D] font-medium text-neutral-600'
+                    }`}>
+                    Eksplor
+                </span>
+            </Link>
+            <Link href={'/pertanyaan-ku'}>
+                <span
+                    className={`text-center text-sm w-1/2 py-[6px] border-b-2 ${
+                        pathname === '/pertanyaan-ku'
+                            ? 'border-accent-purple font-bold'
+                            : 'border-[#2D2D2D] font-medium text-neutral-600'
+                    }`}>
+                    Pertanyaanku
+                </span>
+            </Link>
+        </div>
+    );
+};
+
+export default MobileTabs;
