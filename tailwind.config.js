d93e9e2356e43800e7c0a38925abd498ed6cc168
IrPanTech <irfannkamill@gmail.com>
2023-06-22 16:45:54 +0700

    add radial gradient to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,12 +46,12 @@ module.exports = {
             },
             backgroundImage: {
                 'gradient-purple':
-                    'radial-gradient(60% 60% at 50% 50%, rgba(100, 75, 169, 0.53) 0%, #030317 100%)'
+                    'radial-gradient(60% 60% at 50% 50%, rgba(100, 75, 169, 0.53) 0%, #030317 100%)',
+                'gradient-purple-thin':
+                    'radial-gradient(60% 60% at 50% 50%, rgba(95, 43, 206, 0.5) 0%, rgba(0, 0, 0, 0) 70%)'
             }
         }
     },
     variants: {},
     plugins: [require('@tailwindcss/forms'), require('daisyui')]
 };
-
-// background: radial-gradient(50% 50% at 50% 50%, rgba(100, 75, 169, 0.53) 0%, #030317 100%)
