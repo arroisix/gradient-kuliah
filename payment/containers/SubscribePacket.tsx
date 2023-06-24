886817521216419ec6342e505cfb8dad1006b4dc
IrPanTech <irfannkamill@gmail.com>
2023-06-24 10:04:07 +0700

    change /langganan page

--- payment/containers/SubscribePacket.tsx
+++ payment/containers/SubscribePacket.tsx
@@ -1,80 +1,104 @@
-import SubscribeButton from 'courses/components/LandingPage/Common/SubscribeButton';
-import { formatter } from 'courses/utils';
+import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
+import Button from 'commons/components/elements/Button';
+import useWindowBreakpoints from 'commons/hooks/useWindowBreakpoints';
+import { addZeroBefore, formatter } from 'courses/utils';
+import { useRouter } from 'next/router';
 import { useGetPacketOfferQuery } from 'payment/redux/api/subscriptionApi';
 import { useEffect, useMemo, useState } from 'react';
+import { SlCheck } from 'react-icons/sl';
+import { useSelector } from 'react-redux';
 
 const PacketCard = ({
     data
 }: GradientBaseComponentWithData<PacketOffer>): JSX.Element => {
-    const isHighlighted = useMemo(() => data.order === 1, [data]);
+    const { checkCustomBreakpoints } = useWindowBreakpoints();
+    const isAuthenticated = useSelector(getIsAuthenticated);
+    const isHighlighted = useMemo(() => data.order === 1, [data.order]);
+    const router = useRouter();
+    const currentDate = new Date();
+
+    function handleClick(): void {
+        if (!isAuthenticated) {
+            router.push('/daftar');
+        } else {
+            window.open(
+                `https://api.whatsapp.com/send?phone=6285173430127&text=${encodeURIComponent(
+                    `Halo, Saya tertarik untuk berlangganan ${
+                        data.packet_name
+                    }\n\n[ID:${currentDate.getDate()}${addZeroBefore(
+                        currentDate.getMonth() + 1
+                    )}${currentDate.getFullYear()}]`
+                )}`
+            );
+        }
+    }
 
     return (
         <div
-            className={
+            className={`relative w-[324px] flex flex-col gap-4 md:gap-6 px-6 py-6 md:py-8 rounded-[16px] md:rouded-[20px] ${
                 isHighlighted
-                    ? 'w-full lg:w-fit p-[5px] bg-[linear-gradient(43.82deg,#ECD0CD_0%,#DD837A_28.35%,#CAC7E4_56.15%,#AB8EEC_74.31%)] rounded-[20px] overflow-hidden'
-                    : 'w-full lg:w-fit'
-            }>
+                    ? 'bg-gradient-purple-pricing border-2 border-[#5F2BCE80] rounded-t-none border-t-0 mt-[50px] sm:mt-0'
+                    : 'bg-[#121212]'
+            }`}
+            style={{
+                order: checkCustomBreakpoints(708)
+                    ? isHighlighted
+                        ? 1
+                        : data.order + 2
+                    : data.order
+            }}>
             {isHighlighted && (
-                <div className="flex items-center justify-center w-full p-2 animate-pulse">
-                    <span className="font-bold text-center">
-                        PENAWARAN TERBAIK!
-                    </span>
+                <div className="absolute left-[-2px] top-[-50px] w-[101.3%] h-[50px] bg-[#5F2BCE80] border-2 border-[#5F2BCE80] border-b-0 rounded-b-none rounded-[16px] md:rouded-[20px] py-3 text-center font-bold text-[15px] animate-pulse">
+                    PENAWARAN TERBAIK!
                 </div>
             )}
-            <div
-                className={`flex flex-col gap-2 justify-center items-center px-4 py-8 w-full lg:w-[380px] ${
-                    isHighlighted ? 'rounded-b-[20px]' : 'rounded-[20px]'
-                } backdrop-blur-sm bg-[#121212]`}>
-                <div className="flex flex-col">
-                    <p
-                        className={`font-bold  text-center text-2xl lg:text-3xl
-                        ${
-                            isHighlighted
-                                ? 'gradient-text animate-pulse'
-                                : 'text-white'
-                        }
-                    `}>
-                        {data.packet_name}
-                    </p>
-                    <h3 className="text-xl font-bold text-center line-through lg:text-2xl decoration-2 text-stone-500 decoration-red-600">
-                        {
-                            formatter
-                                .format(
-                                    data.price_before_discount as unknown as number
-                                )
-                                .split(',')[0]
-                        }
-                    </h3>
-                    <div className="relative">
-                        <h1
-                            className={`text-[35px] lg:text-[40px] font-bold text-center font-body ${
-                                data.order === 1
-                                    ? 'gradient-blur-text animate-pulse'
-                                    : 'text-white'
-                            }`}>
-                            {data.is_free
-                                ? 'GRATIS'
-                                : formatter
-                                      .format(data.price as unknown as number)
-                                      .split(',')[0]}
-                        </h1>
-                    </div>
-                </div>
-                <ul className="grid content-center grid-cols-1 py-4">
-                    {data.benefits?.data.map((benefit: string) => (
-                        <li className="font-body lg:text-lg" key={benefit}>
-                            {benefit}
-                        </li>
-                    ))}
-                </ul>
-                <SubscribeButton
-                    packetId={data.id}
-                    className={
-                        isHighlighted ? 'animate-pulse' : '!bg-neutral-900'
+            <div className="flex flex-col md:gap-1 text-center">
+                <h5 className="font-extrabold text-xl md:text-2xl">
+                    {data.packet_name}
+                </h5>
+                <h4
+                    className={`font-body font-bold text-[32px] md:text-[40px] ${
+                        isHighlighted ? 'gradient-text' : ''
+                    }`}>
+                    {
+                        formatter
+                            .format(data.price as unknown as number)
+                            .split(',')[0]
                     }
-                />
+                </h4>
+                <h6 className="text-xl font-bold line-through lg:text-2xl decoration-2 text-stone-500 decoration-red-600">
+                    {
+                        formatter
+                            .format(
+                                data.price_before_discount as unknown as number
+                            )
+                            .split(',')[0]
+                    }
+                </h6>
             </div>
+            <div className="flex flex-col gap-[10px] md:gap-3 h-full">
+                {data?.benefits?.feature?.map(
+                    ({ title, description }, index) => (
+                        <div key={index} className="flex items-center gap-3">
+                            <SlCheck className="text-accent-purple" size={32} />
+                            <div className="flex flex-col">
+                                <span className="inline-block font-extrabold text-sm">
+                                    {title}
+                                </span>
+                                <span className="inline-block font-body text-xs">
+                                    {description}
+                                </span>
+                            </div>
+                        </div>
+                    )
+                )}
+            </div>
+            <Button
+                variant={isHighlighted ? 'primary' : 'custom'}
+                className={`${isHighlighted ? '' : 'bg-[#212121]'}`}
+                onClick={handleClick}>
+                Akses Sekarang
+            </Button>
         </div>
     );
 };
@@ -118,7 +142,7 @@ const SubscribePacket = (): JSX.Element => {
     }, []);
 
     return (
-        <section className="min-h-screen py-24 px-4 md:px-[7.5rem] flex flex-col items-center justify-center gap-8 lg:gap-16">
+        <section className="min-h-screen py-24 px-[18px] flex flex-col items-center justify-center gap-8 lg:gap-16">
             <h4 className="flex flex-col lg:flex-row gap-3 items-center font-bold text-3xl text-center">
                 <span>Promo akan berakhir dalam</span>
                 <div className="grid grid-flow-col gap-1 items-center text-center auto-cols-max">
@@ -156,7 +180,7 @@ const SubscribePacket = (): JSX.Element => {
                     </div>
                 </div>
             </h4>
-            <div className="flex flex-col flex-wrap items-end justify-center w-full gap-4 lg:flex-row">
+            <div className="w-full flex gap-6 justify-center flex-wrap mt-14">
                 {isLoading && (
                     <>
                         <div className="h-[402px] w-full lg:w-[380px] bg-neutral-600 animate-pulse rounded-[20px]" />
