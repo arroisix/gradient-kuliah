a159e166dcac6ecec455e29495f199a0aa0bea43
IrPanTech <irfannkamill@gmail.com>
2023-06-26 22:29:41 +0700

    Create komunitas RTK Query

---
+++ komunitas/types/komunitas.d.ts
@@ -0,0 +1,124 @@
+interface SubjectCategoriesResponse {
+    categories: [
+        {
+            id: string;
+            name: string;
+        }
+    ];
+}
+
+interface CommunityNotification {
+    unseen_comment_counts: number;
+}
+
+interface PostQuestionAnswerPayload {
+    post_id?: string;
+    content: string;
+    category_id: string;
+    attachment_urls: string[];
+}
+
+interface PostQuestionAnswerResponse {
+    id: string;
+    parent_id?: string;
+    content: string;
+    comment_counts: number;
+    viewer_counts: number;
+    category: string;
+    slug?: string;
+    student: {
+        id: string;
+        username: string;
+        photo_url?: string;
+    };
+    created_at: timestamp;
+    student: [
+        {
+            id: string;
+            url: string;
+        }
+    ];
+}
+
+interface CommunityPostQuery {
+    category_id?: string;
+    sort_by?: 'LATEST' | 'POPULAR' | 'ANSWERED' | 'NOT_ANSWERED';
+    user_id?: string;
+}
+
+interface CommunityPostResponse {
+    community_posts: [
+        {
+            id: string;
+            content: string;
+            slug: string;
+            category: string;
+            viewer_counts: number;
+            comment_counts: number;
+            created_at: timestamp;
+            student: {
+                id: string;
+                photo_url: string;
+                username: string;
+            };
+        }
+    ];
+}
+
+interface MyQuestionListResponse {
+    questions: [
+        {
+            date: string;
+            items: [
+                {
+                    id: string;
+                    content: string;
+                    slug: string;
+                    unseen_comment_counts: number;
+                    created_at: timestamp;
+                }
+            ];
+        }
+    ];
+}
+
+interface ExploreQuestionResponse {
+    questions: [
+        {
+            id: string;
+            slug: string;
+            content: string;
+        }
+    ];
+}
+
+interface CommunityPostDetailResponse {
+    id: string;
+    content: string;
+    viewer_counts: number;
+    comment_counts: number;
+    created_at: timestamp;
+    category: string;
+    student: {
+        id: string;
+        photo_url: string;
+        username: string;
+    };
+}
+
+interface CommunityPostCommentDetailResponse {
+    comments: [
+        {
+            id: string;
+            content: string;
+            comment_counts: number;
+            created_at: timestamp;
+            student: {
+                id: string;
+                photo_url: string;
+                username: string;
+                is_expert: boolean;
+            };
+        }
+    ];
+}
