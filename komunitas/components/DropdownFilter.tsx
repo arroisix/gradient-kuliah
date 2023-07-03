4c95fe76e0e991554064c2af5d218c5b6d2117ad
IrPanTech <irfannkamill@gmail.com>
2023-07-03 20:51:40 +0700

    integrate with BE

--- komunitas/components/DropdownFilter.tsx
+++ komunitas/components/DropdownFilter.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-// const OPTIONS = [
-//     { key: '', value: 'Semua' },
-//     { key: 'matematika', value: 'Matematika' },
-//     { key: 'fisika', value: 'Fisika' },
-//     { key: 'kimia', value: 'Kimia' }
-// ];
-
 const DropdownFilter = ({
     onChange,
     options
@@ -19,6 +12,9 @@ const DropdownFilter = ({
             className={`w-1/2 md:w-fit pl-[18px] pr-[50px] border-none bg-[#2C2C2C] rounded-[70px] text-xs font-bold cursor-pointer focus:outline-none focus:ring-0 focus:appearance-none`}
             onChange={onChange}
             name={'name'}>
+            <option className="pt-4" key="" value="" label="Semua">
+                Semua
+            </option>
             {options?.categories?.map((option) => (
                 <option
                     className="pt-4"
--- komunitas/containers/index.tsx
+++ komunitas/containers/index.tsx
@@ -1,3 +1,7 @@
+import AuthContext from 'authentication/contexts/AuthProvider';
+import LoadingBackdrop from 'commons/components/elements/LoadingBackdrop';
+import Skeleton from 'commons/components/elements/Skeleton';
+import useTransition from 'commons/hooks/useTransition';
 import useWindowBreakpoints from 'commons/hooks/useWindowBreakpoints';
 import DropdownFilter from 'komunitas/components/DropdownFilter';
 import DropdownSort from 'komunitas/components/DropdownSort';
@@ -7,50 +11,35 @@ import MobileTabs from 'komunitas/components/MobileTabs';
 import QuestionCard from 'komunitas/components/QuestionCard';
 import {
     useGetCommunityPostQuery,
-    useGetSubjectCategoriesQuery
+    useGetExploreQuestionQuery,
+    useGetMyQuestionListQuery,
+    useGetSubjectCategoriesQuery,
+    usePostQuestionAnswerMutation
 } from 'komunitas/redux/api/komunitasApi';
-import React, { useState } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useContext, useState } from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { CgSearch } from 'react-icons/cg';
-
-// const DUMMY_DATA = {
-//     community_posts: [
-//         {
-//             id: '1',
-//             content: 'haloo',
-//             viewer_counts: 10,
-//             comment_counts: 10,
-//             created_at: 1687229985,
-//             course_name: 'matematika',
-//             student: {
-//                 id: '90',
-//                 photo_url: '',
-//                 username: 'irfan.kamil'
-//             }
-//         },
-//         {
-//             id: '2',
-//             content: 'bang aku ga ngerti',
-//             viewer_counts: 17,
-//             comment_counts: 3,
-//             created_at: 1687229985,
-//             course_name: 'fisika',
-//             student: {
-//                 id: '90',
-//                 photo_url: '',
-//                 username: 'irfan.kamil'
-//             }
-//         }
-//     ],
-//     total_items: 10,
-//     current_page: 10,
-//     items_per_page: 10
-// };
+import { MdChevronRight } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
 const KomunitasContainer = (): JSX.Element => {
     const { isMobileBreakpoints } = useWindowBreakpoints();
+    const router = useRouter();
+    const { pathname } = router;
+    const loadingTransition = useTransition(router);
+    const { profile } = useContext(AuthContext);
 
     const { data: subjects } = useGetSubjectCategoriesQuery();
+    const [postCommunity, { isLoading: isLoadingPost }] =
+        usePostQuestionAnswerMutation();
 
+    const [formContent, setFormContent] = useState('');
+    const [category, setCategory] = useState('');
+    const [attachmentUrl, setAttachmentUrl] = useState<string[]>([]);
+    const [attachmentName, setAttachmentName] = useState<string[]>([]);
     const [search, setSearch] = useState('');
     const [showSort, setShowSort] = useState(false);
     const [filter, setFilter] = useState('');
@@ -58,10 +47,78 @@ const KomunitasContainer = (): JSX.Element => {
         'LATEST' | 'POPULAR' | 'ANSWERED' | 'NOT_ANSWERED'
     >('LATEST');
 
-    const { data } = useGetCommunityPostQuery({
+    const {
+        data,
+        isLoading: isLoadingData,
+        isFetching: isFetchingData
+    } = useGetCommunityPostQuery({
         sort_by: sort,
-        category_id: filter
+        category_id: filter,
+        user_id: pathname.includes('pertanyaan-ku')
+            ? profile?.user_id
+            : undefined
     });
+
+    const {
+        data: myQuestion,
+        isLoading: isLoadingMyQuestion,
+        isFetching: isFetchingMyQuestion
+    } = useGetMyQuestionListQuery(
+        {
+            user_id: profile?.user_id as string
+        },
+        { skip: !profile?.user_id }
+    );
+
+    const {
+        data: sideExploreData,
+        isLoading: isLoadingSideExplore,
+        isFetching: isFetchingSideExplore
+    } = useGetExploreQuestionQuery(
+        {},
+        { skip: !pathname.includes('pertanyaan-ku') }
+    );
+
+    async function handleSubmit(): Promise<void> {
+        if (!category) {
+            toast.error('Kategori tidak boleh kosong', {
+                position: 'top-center',
+                theme: 'colored',
+                hideProgressBar: true,
+                toastId: 'KATEGORI_NULL'
+            });
+            return;
+        }
+
+        const contentwithAttachments =
+            attachmentUrl.length !== 0
+                ? `${formContent}${attachmentUrl.map(
+                      (value) => `\n\n![image](${value})`
+                  )}`
+                : formContent;
+
+        if (!contentwithAttachments) {
+            toast.error('Pertanyaan tidak boleh kosong', {
+                position: 'top-center',
+                theme: 'colored',
+                hideProgressBar: true,
+                toastId: 'KATEGORI_NULL'
+            });
+            return;
+        }
+
+        await postCommunity({
+            post_id: null,
+            content: contentwithAttachments,
+            category_id: category,
+            attachment_urls: attachmentUrl
+        });
+
+        setFormContent('');
+        setCategory('');
+        setAttachmentUrl([]);
+        setAttachmentName([]);
+    }
 
     function handleChangeSearch(
         event: React.ChangeEvent<HTMLInputElement>
@@ -87,11 +144,36 @@ const KomunitasContainer = (): JSX.Element => {
                     placeholder="Cari pertanyaan"
                     onChange={handleChangeSearch}
                     rightIcon={<CgSearch />}
+                    handleSubmit={() => undefined}
                 />
-                <KomunitasForm />
+                <KomunitasForm
+                    formContent={formContent}
+                    setFormContent={setFormContent}
+                    category={category}
+                    setCategory={setCategory}
+                    attachmentUrl={attachmentUrl}
+                    setAttachmentUrl={setAttachmentUrl}
+                    attachmentName={attachmentName}
+                    setAttachmentName={setAttachmentName}
+                    bucketKey="qna"
+                    handleSubmit={handleSubmit}
+                    isUsingCategories={true}
+                    subjectCategories={subjects?.categories}
+                    submitButtonText={
+                        isLoadingPost ? (
+                            <AiOutlineLoading3Quarters className="animate-spin" />
+                        ) : (
+                            'Tanyakan'
+                        )
+                    }
+                />
                 {isMobileBreakpoints && <MobileTabs />}
                 <div className="flex justify-between items-center">
-                    <h2 className="hidden md:block font-extrabold">Eksplor</h2>
+                    <h2 className="hidden md:block font-extrabold">
+                        {pathname.includes('pertanyaan-ku')
+                            ? 'Pertanyaanku'
+                            : 'Eksplor'}
+                    </h2>
                     <div className="flex gap-3 items-center w-full md:w-fit">
                         <DropdownFilter
                             onChange={handleChangeFilter}
@@ -107,16 +189,123 @@ const KomunitasContainer = (): JSX.Element => {
                     </div>
                 </div>
                 <div className="flex flex-col gap-[18px]">
-                    {data?.community_posts.map((value) => (
-                        <QuestionCard
-                            key={value.id}
-                            {...value}
-                            clickable={true}
-                        />
-                    ))}
+                    {isLoadingData || isFetchingData ? (
+                        <>
+                            <Skeleton className="!mb-0 h-40" />
+                            <Skeleton className="!mb-0 h-40" />
+                            <Skeleton className="!mb-0 h-40" />
+                        </>
+                    ) : (
+                        data?.community_posts.map((value) => (
+                            <QuestionCard
+                                key={value.id}
+                                {...value}
+                                clickable={true}
+                            />
+                        ))
+                    )}
                 </div>
             </div>
-            <div className="hidden md:block w-full lg:w-4/12">Pertanyaanku</div>
+            <div className="relative w-screen md:w-full lg:w-4/12">
+                <div className="hidden md:block lg:fixed lg:w-[30%] lg:right-[2%] h-[85vh] bg-[#121212] ml-[-16px] mb-[-40px] md:m-0 px-[18px] py-5 md:rounded-lg overflow-hidden">
+                    <h4 className="font-extrabold pb-[20px]">
+                        {pathname.includes('pertanyaan-ku')
+                            ? 'Eksplor'
+                            : 'Pertanyaanku'}
+                    </h4>
+                    <div className="flex flex-col gap-[18px]">
+                        {pathname.includes('pertanyaan-ku') ? (
+                            isLoadingSideExplore || isFetchingSideExplore ? (
+                                <>
+                                    <Skeleton className="h-3 !mb-0" />
+                                    <Skeleton className="h-3 !mb-0" />
+                                    <Skeleton className="h-3 !mb-0" />
+                                </>
+                            ) : (
+                                <div className="flex flex-col gap-2 px-[10px] py-[10px] bg-[#1D1D1D] rounded">
+                                    {sideExploreData?.questions?.map(
+                                        ({ slug, content }) => (
+                                            <Link
+                                                key={slug}
+                                                href={`/komunitas/${slug}`}>
+                                                <div className="flex justify-between items-center gap-2 py-1 cursor-pointer z-[1]">
+                                                    <span className="text-xs whitespace-nowrap text-ellipsis overflow-hidden">
+                                                        {content}
+                                                    </span>
+                                                    <div>
+                                                        <MdChevronRight
+                                                            className="text-neutral-600"
+                                                            size={18}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        )
+                                    )}
+                                </div>
+                            )
+                        ) : isLoadingMyQuestion ||
+                          isFetchingMyQuestion ||
+                          !profile ? (
+                            <>
+                                <Skeleton className="h-3 !mb-0" />
+                                <Skeleton className="h-3 !mb-0" />
+                                <Skeleton className="h-3 !mb-0" />
+                            </>
+                        ) : (
+                            myQuestion?.questions?.map((value) => (
+                                <div
+                                    key={value.date}
+                                    className="flex flex-col gap-2">
+                                    <span className="font-body text-xs text-neutral-600">
+                                        {moment(value.date).fromNow()}
+                                    </span>
+                                    <div className="flex flex-col gap-2 px-[10px] py-[10px] bg-[#1D1D1D] rounded">
+                                        {value.items.map(
+                                            ({ slug, content, id }) => (
+                                                <Link
+                                                    key={id}
+                                                    href={`/komunitas/${slug}`}>
+                                                    <div className="flex justify-between items-center gap-2 py-1 cursor-pointer z-[1]">
+                                                        <span className="text-xs whitespace-nowrap text-ellipsis overflow-hidden">
+                                                            {content}
+                                                        </span>
+                                                        <div>
+                                                            <MdChevronRight
+                                                                className="text-neutral-600"
+                                                                size={18}
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </Link>
+                                            )
+                                        )}
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
+                    <div className="absolute w-full h-full left-0 top-0">
+                        <div className="absolute bottom-0 left-0 w-full h-[150px] bg-gradient-to-b from-transparent via-[#121212] to-[#121212] z-[1]"></div>
+                        <div className="absolute bottom-0 left-0 w-full px-[18px] z-[1]">
+                            <Link
+                                href={
+                                    pathname.includes('pertanyaan-ku')
+                                        ? '/komunitas'
+                                        : '/komunitas/pertanyaan-ku'
+                                }>
+                                <button className="bg-neutral-800 font-extrabold text-xs w-full py-2 rounded-[70px]">
+                                    {pathname.includes('pertanyaan-ku')
+                                        ? 'Lihat di Komunitas'
+                                        : 'Lihat Semua'}
+                                </button>
+                            </Link>
+                            <div className="w-full h-[48px] md:h-[20px] bg-[#121212]"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {loadingTransition && <LoadingBackdrop />}
         </section>
     );
 };
