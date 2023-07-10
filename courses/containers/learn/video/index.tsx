14a0d06ab44847b8ea0b4bfba555347150354b74
IrPanTech <irfannkamill@gmail.com>
2023-07-10 21:54:05 +0700

    improve watching experience UI

--- courses/containers/learn/video/index.tsx
+++ courses/containers/learn/video/index.tsx
@@ -1,15 +1,12 @@
 import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
-import useWindowBreakpoints from 'commons/hooks/useWindowBreakpoints';
-import LearnContentBox from 'courses/components/LearnContentBox';
 import AiTutor from 'courses/components/LearningExperience/AiTutor';
 import { useLearning } from 'courses/contexts/LearningProvider';
 import useCourseSubscription from 'courses/hooks/useCourseSubscription';
 import { useGetSubchapterDetailQuery } from 'courses/redux/api/privateCourseApi';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import LearnVideo from './learnVideo';
+import CourseDetailBox from 'courses/components/CourseDetailBox';
 
 const VideoLearnContainer = ({
     chapters
@@ -28,70 +25,34 @@ const VideoLearnContainer = ({
     const { data, isLoading } = useGetSubchapterDetailQuery(sub as string, {
         skip: sub === null || sub === undefined || !isAuthenticated
     });
-    const { isMobileBreakpoints } = useWindowBreakpoints();
-    const [showMaterial, setShowMaterial] = useState(false);
     const { video } = useLearning();
 
     return (
-        <section className="pt-[65px] flex flex-col md:flex-row relative md:overflow-hidden min-h-[100vh] md:h-screen">
-            <div className="w-full h-full">
-                {!isLoading ? (
-                    data ? (
-                        <LearnVideo
-                            key={data.id}
-                            learningProgress={{
-                                id: learning_progress_id as string,
-                                watch_progress:
-                                    watch_progress as SubchapterProgress[],
-                                latest_watch_video:
-                                    latest_watch_video as SubchapterProgress
-                            }}
-                        />
+        <section className="relative pt-[97px] min-h-[100vh]">
+            <div className="w-full flex gap-8 px-16">
+                <div className="w-full lg:w-[70%] h-full rounded-lg overflow-hidden">
+                    {!isLoading ? (
+                        data ? (
+                            <LearnVideo
+                                key={data.id}
+                                learningProgress={{
+                                    id: learning_progress_id as string,
+                                    watch_progress:
+                                        watch_progress as SubchapterProgress[],
+                                    latest_watch_video:
+                                        latest_watch_video as SubchapterProgress
+                                }}
+                            />
+                        ) : (
+                            <></>
+                        )
                     ) : (
-                        <></>
-                    )
-                ) : (
-                    <div className="w-full h-3/4 bg-neutral-600 animate-pulse" />
-                )}
-            </div>
-            <div
-                className="md:hidden bg-neutral-800 border-4 border-neutral-600 text-neutral-200 top-[65px] right-0 w-8 rounded-l-xl h-16 z-10 fixed flex justify-center items-center"
-                onClick={() => setShowMaterial(true)}
-                aria-hidden>
-                <FaChevronLeft />
-                <FaChevronLeft className="-ml-2" />
-            </div>
-            {isMobileBreakpoints && id && showMaterial && (
-                <div className="fixed z-[100] top-0 right-0 w-screen h-[calc(100vh-64px)] bg-neutral-900">
-                    <header className="w-full px-4 md:px-8 py-4 flex items-center justify-between">
-                        <span className="text-2xl font-bold cursor-pointer font-[Urbanist]">
-                            Gradient
-                        </span>
-                        <div
-                            className="flex items-center"
-                            onClick={() => setShowMaterial(false)}
-                            aria-hidden>
-                            <FaChevronRight />
-                            <FaChevronRight className="-ml-2" />
-                            <span className="text-bold">Tutup</span>
-                        </div>
-                    </header>
-                    <LearnContentBox
-                        extraCallback={() => setShowMaterial(false)}
-                        slug={id as string}
-                        chapters={chapters}
-                        isSubscribed={is_subscribed}
-                    />
+                        <div className="w-full h-full bg-neutral-600 animate-pulse" />
+                    )}
                 </div>
-            )}
-            <div className="w-full md:w-[30vw] hidden md:block">
-                {id && (
-                    <LearnContentBox
-                        slug={id as string}
-                        chapters={chapters}
-                        isSubscribed={is_subscribed}
-                    />
-                )}
+                <div className="w-[30%] hidden lg:block">
+                    <CourseDetailBox />
+                </div>
             </div>
             {video?.ai_unique_id && <AiTutor uniqueId={video.ai_unique_id} />}
         </section>
--- courses/containers/learn/video/learnVideo.tsx
+++ courses/containers/learn/video/learnVideo.tsx
@@ -1,7 +1,6 @@
 import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
 import VideoPlayer from 'commons/components/elements/Video';
 import PopupQuestionContent from 'courses/components/Exercise/PopupQuestion';
-import LearningExperience from 'courses/components/LearningExperience';
 import NeedSubscribe from 'courses/components/NeedSubscribe';
 import { useLearning } from 'courses/contexts/LearningProvider';
 import { useTrackSubchapterProgressMutation } from 'courses/redux/api/learningExperienceApi';
@@ -57,7 +56,6 @@ const LearnVideo = ({
     return (
         <div className="w-full h-full transition-all overflow-x-hidden relative">
             {renderVideoPlayer()}
-            <LearningExperience />
         </div>
     );
 };
