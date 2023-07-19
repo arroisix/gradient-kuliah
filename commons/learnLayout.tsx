67b7f2a526642f946eacee42cca0af871f0a36b9
IrPanTech <irfannkamill@gmail.com>
2023-07-19 09:12:29 +0700

    give padding

--- commons/learnLayout.tsx
+++ commons/learnLayout.tsx
@@ -42,7 +42,7 @@ const LearnLayout = ({
                 className={`min-h-screen ${
                     showSidebar && isAuthenticated
                         ? 'pt-24 pb-10 px-4 md:px-0 flex gap-[2rem] lg:gap-[6rem]'
-                        : ''
+                        : 'pt-24'
                 }`}>
                 {showSidebar && isAuthenticated && (
                     <Sidebar fullHeight={fullHeightSidebar} />
