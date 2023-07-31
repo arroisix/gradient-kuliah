56ea4729f7b53d1e376860a149842e796423e536
IrPanTech <irfannkamill@gmail.com>
2023-07-31 16:36:15 +0700

    create referal home page

---
+++ pages/referal/index.tsx
@@ -0,0 +1,12 @@
+import LearnLayout from 'commons/learnLayout';
+import ReferalContainer from 'referal/containers';
+
+const Referral = (): JSX.Element => {
+    return (
+        <LearnLayout>
+            <ReferalContainer />
+        </LearnLayout>
+    );
+};
+
+export default Referral;
