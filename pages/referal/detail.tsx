131295f4ac2864032d729d74cb2536a9cc532a1d
IrPanTech <irfannkamill@gmail.com>
2023-07-31 16:36:22 +0700

    create referral detail page

---
+++ pages/referal/detail.tsx
@@ -0,0 +1,12 @@
+import LearnLayout from 'commons/learnLayout';
+import ReferalDetailContainer from 'referal/containers/detail';
+
+const ReferalDetail = (): JSX.Element => {
+    return (
+        <LearnLayout>
+            <ReferalDetailContainer />
+        </LearnLayout>
+    );
+};
+
+export default ReferalDetail;
