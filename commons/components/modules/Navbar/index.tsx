8444b687faa0c25e2aa83193f147dd29e715bd14
IrPanTech <irfannkamill@gmail.com>
2023-08-02 09:47:12 +0700

    add kelas to navbar

--- commons/components/modules/Navbar/index.tsx
+++ commons/components/modules/Navbar/index.tsx
@@ -219,15 +219,24 @@ const Navbar = ({
                             )}
                             {isAuthenticated ? (
                                 <nav
-                                    className={`ml-12 cursor-pointer hover:text-accent-blue relative ${
-                                        router.pathname === '/dashboard' &&
-                                        'text-accent-blue'
-                                    } ${
-                                        isProfileHovered && 'text-accent-blue'
-                                    }`}
-                                    onMouseEnter={onMouseEnterProfile}
-                                    onMouseLeave={onMouseLeaveProfile}>
-                                    <div className="flex items-center gap-2">
+                                    className={`ml-12 flex gap-6 cursor-pointer relative`}>
+                                    <Link href="/kelas">
+                                        <nav
+                                            className="ml-12 cursor-pointer hover:text-accent-blue"
+                                            onMouseEnter={onMouseEnterOther}>
+                                            Kelas
+                                        </nav>
+                                    </Link>
+                                    <div
+                                        className={`flex items-center gap-2 hover:text-accent-blue ${
+                                            router.pathname === '/dashboard' &&
+                                            'text-accent-blue'
+                                        } ${
+                                            isProfileHovered &&
+                                            'text-accent-blue'
+                                        }`}
+                                        onMouseEnter={onMouseEnterProfile}
+                                        onMouseLeave={onMouseLeaveProfile}>
                                         {!profile ? (
                                             <div className="w-[23px] h-[23px] bg-neutral-600 animate-pulse rounded-full"></div>
                                         ) : !!profile.photo_profile ? (
