2e1c5386a876787e394801870d45bbdb43fe7805
IrPanTech <irfannkamill@gmail.com>
2023-08-10 00:33:03 +0700

    improve payment history

--- payment/containers/TransactionListContainer.tsx
+++ payment/containers/TransactionListContainer.tsx
@@ -6,16 +6,20 @@ const TransactionListContainer = (): JSX.Element => {
 
     return (
         <section className="min-h-screen pt-24 px-4 md:px-[7.5rem]">
-            <h1 className="text-3xl md:text-5xl font-bold mb-16">
+            <h1 className="sm:text-2xl text-center font-bold mb-6 sm:mb-8">
                 Riwayat Pembelian
             </h1>
-            {(data?.data as [Transaction])?.map((transaction: Transaction) => (
-                <TransactionCard
-                    isList
-                    transaction={transaction}
-                    key={transaction.id}
-                />
-            ))}
+            <div className="flex flex-col gap-[18px]">
+                {(data?.data as [Transaction])?.map(
+                    (transaction: Transaction) => (
+                        <TransactionCard
+                            isList
+                            transaction={transaction}
+                            key={transaction.id}
+                        />
+                    )
+                )}
+            </div>
             <div className="mb-16" />
         </section>
     );
