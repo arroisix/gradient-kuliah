ebdefdd55c98186d3b2df2572be4d2f1086d4dd1
IrPanTech <irfannkamill@gmail.com>
2023-08-10 09:58:29 +0700

    revert logic

--- payment/components/TransactionCard.tsx
+++ payment/components/TransactionCard.tsx
@@ -159,14 +159,14 @@ const TransactionCard = ({
 
     function activeTransaction(): boolean | string {
         if (transaction.status === 'SUCCESS') {
-            if (expiryDay <= 14) {
-                return 'Perpanjang';
-            } else if (
+            if (
                 moment(moment(transaction.created_at).startOf('day'))
                     .add(subscribed_packet?.active_duration, 'd')
                     .unix() < moment().unix()
             ) {
                 return 'Perbarui';
+            } else if (expiryDay <= 14) {
+                return 'Perpanjang';
             }
         }
 
