f7a95bc775084bb2c60fb2add5616d28560c6431
Litha <asfiolitha.wilmarani@ecomindo.com>
2023-09-27 14:17:39 +0700

    update list of content styling similar to bookmark

--- courses/components/LearningExperience/AstroNotes/ListOfContents/SubChapterContentItem.tsx
+++ courses/components/LearningExperience/AstroNotes/ListOfContents/SubChapterContentItem.tsx
@@ -35,7 +35,7 @@ export const SubChapterContentItem = ({
                     href={`/astronotes/${slug}/${subchapter.page_order}#${subchapter.id}`}
                     scroll={false}
                     key={subchapter.id}>
-                    <a className="text-black dark:text-[#CCCCCC] p-1 cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-700 rounded">
+                    <a className="p-1 transition cursor-pointer text-neutral-600 dark:text-neutral-400 btn-ghost rounded-btn">
                         <ReactMarkdown
                             className="markdown-body-sm markdown-overflow-break-word markdown-blue-link font-body markdown-img-max-height"
                             remarkPlugins={[remarkMath, remarkGfm]}
