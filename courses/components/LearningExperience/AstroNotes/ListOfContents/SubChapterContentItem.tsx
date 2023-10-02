103b035b258e6f261361cff161791e05efdf3029
asfiowilma <56245115+asfiowilma@users.noreply.github.com>
2023-10-02 14:29:26 +0700

    Round Corners on Images and Tables (#135)
    * fix anchor tag inside Link element
    
    * fix latex overflow outside callout and blockquote
    
    * fix overflow sideways
    
    * fix sticky not working when overflow set to non default value
    
    * round corners on images and tables
    
    ---------
    
    Co-authored-by: Litha <asfiolithaw@gmail.com>

--- courses/components/LearningExperience/AstroNotes/ListOfContents/SubChapterContentItem.tsx
+++ courses/components/LearningExperience/AstroNotes/ListOfContents/SubChapterContentItem.tsx
@@ -34,10 +34,10 @@ export const SubChapterContentItem = ({
         <>
             {subchapters?.data.map((subchapter: BookSubchapter) => (
                 <Link
-                    className="p-1 transition cursor-pointer text-neutral-600 dark:text-neutral-400 btn-ghost rounded-btn"
                     href={`/astronotes/${slug}/${subchapter.page_order}#${subchapter.id}`}
                     scroll={false}
                     key={subchapter.id}
+                    className="p-1 transition cursor-pointer text-neutral-600 dark:text-neutral-400 btn-ghost rounded-btn"
                     onClick={() => onClick?.(subchapter)}>
                     <ReactMarkdown
                         className="markdown-body-sm markdown-overflow-break-word markdown-blue-link font-body markdown-img-max-height"
