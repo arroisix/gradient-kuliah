b0bf7ddc0102cea97e2604302ffd5f5785d2d581
Litha <asfiolitha.wilmarani@ecomindo.com>
2023-11-21 16:38:54 +0700

    route matcher to middleware

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,24 @@ import { NextRequest, NextResponse } from 'next/server';
 const COOKIE = 'visitor_id';
 const ACTIVE_AB_TESTING_PAGES = ['/', '/komunitas']; // Add as needed
 
+export const config = {
+    matcher: ['/', '/komunitas']
+};
+
 export async function middleware(req: NextRequest): Promise<NextResponse> {
+    // We only want to run the A/B test on the homepage
+    const pathname = req.nextUrl.pathname;
+    console.log('🚀 MIDDLEWARE CALLED', req.nextUrl.pathname);
+    if (!ACTIVE_AB_TESTING_PAGES.includes(pathname)) {
+        return NextResponse.next();
+    }
+
     // Get existing visitor cookie or create a new one
-    const visitor_id = req.cookies[COOKIE] || crypto.randomUUID();
+    const visitor_id = req.cookies.get(COOKIE) || crypto.randomUUID();
 
     // Setup GrowthBook instance
     growthbook.setFeatures((await getFeatures()) || {});
     growthbook.setAttributes({ id: visitor_id });
-
-    // We only want to run the A/B test on the homepage
-    const pathname = req.nextUrl.pathname;
-    if (!ACTIVE_AB_TESTING_PAGES.includes(pathname)) {
-        return NextResponse.next();
-    }
 
     // Pick which page to render depending on a feature flag
     let res = NextResponse.next();
@@ -35,8 +40,8 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
     }
 
     // Store the visitor cookie if not already there
-    if (!req.cookies[COOKIE]) {
-        res.cookie(COOKIE, visitor_id);
+    if (!req.cookies.get(COOKIE)) {
+        res.cookies.set(COOKIE, visitor_id);
     }
 
     return res;
