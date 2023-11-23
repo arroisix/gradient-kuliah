fda3345729640c839e868af851f4e33231c0752b
Litha <asfiolitha.wilmarani@ecomindo.com>
2023-11-23 13:39:39 +0700

    add bottom bar to community public

--- courses/components/CourseDetailBox/SearchList/index.tsx
+++ courses/components/CourseDetailBox/SearchList/index.tsx
@@ -32,7 +32,7 @@ const SearchList = ({
     const router = useRouter();
     const { id } = router.query;
     const [navigation, setNavigation] = useState<'SEARCH_LIST' | 'SUBCHAPTER'>(
-        'SUBCHAPTER'
+        'SEARCH_LIST'
     );
     const [chapterDetail, setChapterDetail] = useState<CourseChapter>(
         {} as CourseChapter
