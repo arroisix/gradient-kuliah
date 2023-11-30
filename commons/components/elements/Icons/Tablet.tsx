eff2598de19d48f199361db6cade47e6527551fc
zakybilfagih <mackarelfish93@gmail.com>
2023-11-30 11:20:50 +0700

    chore: add icons for keluar perangkat page

---
+++ commons/components/elements/Icons/Tablet.tsx
@@ -0,0 +1,28 @@
+const Tablet = (): JSX.Element => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="44"
+            fill="none">
+            <path
+                fill="#8C8C8C"
+                fillRule="evenodd"
+                d="M0 41.472V1.812C0 .813.812 0 1.813 0h26.374C29.188 0 30 .811 30 1.813v39.659a1.813 1.813 0 01-1.813 1.813H1.813A1.813 1.813 0 010 41.472z"
+                clipRule="evenodd"></path>
+            <path
+                fill="#fff"
+                fillRule="evenodd"
+                d="M.164 41.473V1.813C.164.903.902.165 1.812.165h26.374c.91 0 1.648.738 1.648 1.648v39.66c0 .91-.738 1.648-1.648 1.648H1.812c-.91 0-1.648-.738-1.648-1.648z"
+                clipRule="evenodd"></path>
+            <path
+                fill="#8C8C8C"
+                fillRule="evenodd"
+                d="M15.196 1.845a.198.198 0 10-.395 0 .198.198 0 00.395 0zm-.195 38.801a.923.923 0 110 1.846.923.923 0 010-1.846zm0 .115a.808.808 0 110 1.616.808.808 0 010-1.616z"
+                clipRule="evenodd"></path>
+            <path fill="#B9B9B9" d="M1.254 39.973V3.313h27.494v36.66z"></path>
+        </svg>
+    );
+};
+
+export default Tablet;
