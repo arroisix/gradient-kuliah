15e965949b55b693f3b5fe667586f9291c12e17d
anggardhanoano <anggardhanoano@gmail.com>
2023-12-04 10:14:31 +0700

    update tnc

--- legal/components/SectionContent.tsx
+++ legal/components/SectionContent.tsx
@@ -10,7 +10,12 @@ const SectionContent = ({
     return (
         <div id={slugify(heading)} className="divide-y scroll-mt-20">
             <h2 className="pb-3 font-sans text-xl font-extrabold">{heading}</h2>
-            <p className="pt-5 text-justify">{content}</p>
+            <p
+                className="pt-5 text-justify"
+                dangerouslySetInnerHTML={{
+                    __html: content
+                }}
+            />
         </div>
     );
 };
