893f0e20c11d6a64bb3cb9ab22716155c20abcce
asfiowilma <56245115+asfiowilma@users.noreply.github.com>
2024-01-19 22:45:16 +0700

    Bugfix Transaction History Display (#235)
    * hide perpanjang, perbarui, beli lagi button when upcoming subscription exists
    
    * add redirect to whatsapp
    
    * fix lint error on pnpm-lock
    
    ---------
    
    Co-authored-by: Litha <asfiolitha.wilmarani@ecomindo.com>
    Co-authored-by: Anggardha Febriano <44425096+anggardhanoano@users.noreply.github.com>

--- pages/pembayaran/index.tsx
+++ pages/pembayaran/index.tsx
@@ -3,16 +3,59 @@ import withAuth from 'commons/withAuth';
 import Layout from 'commons/layout';
 import SubscriptionContainer from 'payment/containers';
 import { PaymentProvider } from 'payment/contexts/PaymentProvider';
+import { useFeatureIsOn } from '@growthbook/growthbook-react';
+import { useEffect } from 'react';
+import { useGetDetailPacketOfferQuery } from 'payment/redux/api/subscriptionApi';
+import { useSelector } from 'react-redux';
+import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
+import Image from 'next/image';
+import { CDN_URL } from 'commons/constants';
 
 const Payment = (): JSX.Element => {
     const router = useRouter();
-    const { packetId } = router.query;
+    const { packetId, subscriptionId } = router.query;
+    const isSubscribeViaWhatsapp = useFeatureIsOn('subscribe-via-wa');
+    const isAuthenticated = useSelector(getIsAuthenticated);
+    const { data: packet } = useGetDetailPacketOfferQuery(packetId as string);
+
+    useEffect(() => {
+        if (
+            isSubscribeViaWhatsapp &&
+            isAuthenticated &&
+            packet &&
+            !subscriptionId
+        ) {
+            // redirect to WhatsApp
+            const message = `Halo, saya tertarik untuk berlangganan ${
+                packet?.packet_name ?? ''
+            }`;
+
+            router.push(
+                `https://api.whatsapp.com/send?phone=6285173430127&text=${message}`
+            );
+        }
+    }, [isSubscribeViaWhatsapp, isAuthenticated, packet, subscriptionId]);
 
     return (
         <Layout paymentPage isFullBlackBackground>
             {packetId ? (
                 <PaymentProvider packetId={packetId as string}>
-                    <SubscriptionContainer />
+                    {isSubscribeViaWhatsapp &&
+                    isAuthenticated &&
+                    !subscriptionId ? (
+                        <section className="flex flex-col items-center justify-center h-screen gap-6 text-center">
+                            <Image
+                                src={`${CDN_URL}/assets/redirect_asset.png`}
+                                width={197}
+                                height={118}
+                            />
+                            <p className="text-lg font-bold">
+                                Kamu akan diarahkan ke admin gradient...
+                            </p>
+                        </section>
+                    ) : (
+                        <SubscriptionContainer />
+                    )}
                 </PaymentProvider>
             ) : (
                 <div></div>
--- types/growthbook.d.ts
+++ types/growthbook.d.ts
@@ -1,4 +1,5 @@
 interface GrowthbookFeatures {
     'landing-page-revamp'?: boolean;
     'legal-pages'?: boolean;
+    'subscribe-via-wa'?: boolean;
 }
