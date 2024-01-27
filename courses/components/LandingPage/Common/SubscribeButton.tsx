bddeed20637bb20d3b4c0d97f8270e126d7eaa17
anggardhanoano <anggardhanoano@gmail.com>
2024-01-27 08:35:32 +0700

    temporary CS to Line

--- courses/components/LandingPage/Common/SubscribeButton.tsx
+++ courses/components/LandingPage/Common/SubscribeButton.tsx
@@ -43,7 +43,7 @@ const SubscribeButton = ({
                     href={
                         packetId
                             ? getCSChatRoom(
-                                  'WA',
+                                  'LINE',
                                   encodeURIComponent(
                                       `Halo,saya tertarik untuk berlangganan ${
                                           packet?.packet_name
--- payment/components/OtherPayment.tsx
+++ payment/components/OtherPayment.tsx
@@ -30,7 +30,7 @@ const OtherPaymentBox = forwardRef<HTMLDivElement>(function OtherPaymentBox(
                     onClick={() =>
                         window.open(
                             getCSChatRoom(
-                                'WA',
+                                'LINE',
                                 encodeURIComponent(
                                     `Halo, Saya tertarik untuk berlangganan\n\n[ID:${currentDate.getDate()}${addZeroBefore(
                                         currentDate.getMonth() + 1
