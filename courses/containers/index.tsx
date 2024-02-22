765e45f9d93c3e113da21e089fe519651d619d0d
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-02-22 09:45:08 +0700

    create renew subscribe banner

--- courses/containers/index.tsx
+++ courses/containers/index.tsx
@@ -71,6 +71,12 @@ const ClassContainer = (): JSX.Element => {
     
     const [isSortMenuVisible, setIsSortMenuVisible] = useState(false);
 
+    const [isSubscribe, setIsSubscribe] = useState<boolean>(true);
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <>
             <div
@@ -168,6 +174,23 @@ const ClassContainer = (): JSX.Element => {
                     ))}
                 </Tabs>
             </div>
+            {!isSubscribe && (
+                <div className="fixed bottom-[85px] md:bottom-[32px] left-[12px] md:left-[286px] right-[12px] md:right-[36px] bg-[#B73E32] px-6 py-4 rounded-xl">
+                    <div className="flex justify-between items-center w-full">
+                        <div className="text-white">
+                            <p className="text-lg font-semibold">
+                                Beli & akses seluruh video kelas
+                            </p>
+                            <p className="text-md">
+                                Mulai dari Rp108.000/bulan
+                            </p>
+                        </div>
+                        <button className="py-2 px-6 bg-white font-semibold text-black rounded-full">
+                            Beli Paket
+                        </button>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
