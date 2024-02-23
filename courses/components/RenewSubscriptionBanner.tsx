cf3333e6870101ce13a37935e5b7f566841e3115
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-02-23 16:38:27 +0700

    fixed subscribe flow

--- courses/components/RenewSubscriptionBanner.tsx
+++ courses/components/RenewSubscriptionBanner.tsx
@@ -5,21 +5,22 @@ export default function RenewSubscriptionBanner() {
     const isAuthenticated = useSelector(getIsAuthenticated);
 
     return (
-        <div className="fixed bottom-[85px] md:bottom-[32px] left-[12px] md:left-[286px] right-[12px] md:right-[36px] bg-[#B73E32] px-6 py-4 rounded-xl">
+        <div className="fixed bottom-[85px] md:bottom-[32px] left-[12px] md:left-[286px] right-[12px] md:right-[36px] bg-[#B73E32] px-4 md:px-6 py-3 md:py-4 rounded-lg">
             <div className="flex justify-between items-center w-full">
                 <div className="text-white">
-                    <p className="text-lg font-semibold">
+                    <p className="md:text-lg text-sm font-semibold pb-[2px]">
                         Beli & akses seluruh video kelas
                     </p>
-                    <p className="text-md">Mulai dari Rp108.000/bulan</p>
+                    <p className="md:text-md text-xs">Mulai dari Rp108.000/bulan</p>
                 </div>
                 <button
-                    className="py-2 px-6 bg-white font-semibold text-black rounded-full"
+                    className="py-2 px-6 bg-white font-semibold text-black rounded-full text-sm md:text-md"
                     onClick={() => {
                         if (isAuthenticated)
                             window.location.href = '/langganan';
-                        else window.location.href = '/masuk';
+                        else window.location.href = '/daftar';
                     }}>
+                        
                     <p className="hidden md:block">Beli Paket</p>
                     <p className="md:hidden">Beli</p>
                 </button>
