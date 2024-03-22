e63ccf0c9554a0d0d8a09f896eca2a643078194a
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-03-22 10:24:18 +0700

    fix: renew subscription banner on top of filter

--- courses/components/RenewSubscriptionBanner.tsx
+++ courses/components/RenewSubscriptionBanner.tsx
@@ -5,7 +5,8 @@ export default function RenewSubscriptionBanner() {
     const isAuthenticated = useSelector(getIsAuthenticated);
 
     return (
-        <div className="fixed bottom-[85px] md:bottom-[32px] left-[12px] md:left-[286px] right-[12px] md:right-[36px] bg-[#B73E32] px-4 md:px-6 py-3 md:py-4 rounded-lg">
+        <div className="fixed bottom-[85px] md:bottom-[32px] left-[12px] md:left-[286px] right-[12px] md:right-[36px] bg-[#B73E32] px-4 md:px-6 py-3 md:py-4 rounded-lg"
+            style={{zIndex: 15}}>
             <div className="flex justify-between items-center w-full">
                 <div className="text-white">
                     <p className="md:text-lg text-sm font-semibold pb-[2px]">
