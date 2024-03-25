546d0622b5f289429d873052b115d93642e35b2b
Litha <asfiolitha.wilmarani@ecomindo.com>
2024-03-26 05:11:52 +0700

    move dashboard tour config

--- dashboard/containers/dashboardContent.tsx
+++ dashboard/containers/dashboardContent.tsx
@@ -18,6 +18,7 @@ import { useLocalStorage } from 'usehooks-ts';
 import useDriver from 'library/driver.js/useDriver';
 import 'driver.js/dist/driver.css';
 import { FiChevronRight } from 'react-icons/fi';
+import { dashboardTourConfig } from './../constants/dashboard-tour';
 
 const DashboardContent = (): JSX.Element => {
     const isAuthenticated = useSelector(getIsAuthenticated);
@@ -40,46 +41,7 @@ const DashboardContent = (): JSX.Element => {
 
     const [tourViewed, setTourViewed] = useLocalStorage('tourViewed', false);
     const driver = useDriver({
-        popoverClass: 'dashboard-tour',
-        showProgress: true,
-        showButtons: ['next'],
-        steps: [
-            {
-                element: '[data-tour="step-1"]',
-                popover: {
-                    description:
-                        'Selamat datang! Mulai kelas pertamamu di sini',
-                    side: 'top',
-                    align: 'center'
-                }
-            },
-            {
-                element: '[data-tour="step-2"]',
-                popover: {
-                    description:
-                        'Kerjain latihan soal, lengkap sama jawabannya',
-                    side: 'top',
-                    align: 'center'
-                }
-            },
-            {
-                element: '[data-tour="step-3"]',
-                popover: {
-                    description: 'Males nyatet? Baca rangkuman aja!',
-                    side: 'top',
-                    align: 'center'
-                }
-            },
-            {
-                element: '[data-tour="step-4"]',
-                popover: {
-                    description:
-                        'Bingung? Tanya di Gradient, pasti dapet jawaban',
-                    side: 'top',
-                    align: 'end'
-                }
-            }
-        ],
+        ...dashboardTourConfig,
         onPopoverRender: (popover, { config, state }) => {
             if (state.activeIndex == 0) {
                 const skipButton = document.createElement('button');
