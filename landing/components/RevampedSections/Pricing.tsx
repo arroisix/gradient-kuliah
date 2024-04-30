cc7394f744194e049e0117e965e4ac42863fc08d
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2024-04-30 20:24:49 +0700

    fix: fix login redirect from landing page

--- landing/components/RevampedSections/Pricing.tsx
+++ landing/components/RevampedSections/Pricing.tsx
@@ -24,7 +24,7 @@ const Pricing = ({ pricingData, ctaEventName }: PricingProps): JSX.Element => {
             <Paywall
                 pricingData={pricingData}
                 ctaEventName={ctaEventName}
-                redirect="/transaksi"
+                redirect="/pembayaran"
                 isCarousel={isTabletBreakpoints}
             />
         </Container>
