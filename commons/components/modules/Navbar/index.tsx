523a5e61a8e30c659bd6f572ce68ac143b27e5a5
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-05-29 20:22:53 +0700

    feat(landing page): fixed padding in integration between navbar and learn layout

--- commons/components/modules/Navbar/index.tsx
+++ commons/components/modules/Navbar/index.tsx
@@ -188,7 +188,7 @@ const Navbar = ({
             className={`fixed top-0 left-0 w-full z-20 ${computeBgColor()} transition-all ease-in-out duration-200 flex flex-col`}
             onMouseEnter={() => setNavbarHovered(true)}
             onMouseLeave={onMouseLeaveNavbar}>
-            <div className="flex items-center justify-between w-full px-4 py-3 md:px-8 lg:px-20">
+            <div className="flex items-center justify-between w-full px-4 py-3 md:px-8 lg:px-24">
                 <div className="flex items-center gap-4">
                     {(isLandingPageRevampOn ||
                         (!isLandingPageRevampOn && isAuthenticated)) &&
--- commons/learnLayout.tsx
+++ commons/learnLayout.tsx
@@ -81,8 +81,9 @@ const LearnLayout = ({
                             'w-full',
                             showSidebar &&
                                 fullHeightSidebar &&
-                                is_subscribed &&
-                                'md:ml-[250px]'
+                                is_subscribed ?
+                                    'md:ml-[250px]' :
+                                    'lg:px-16 xl:px-12'
                         )}>
                         {isAuthenticated &&
                             isCoursePage &&
