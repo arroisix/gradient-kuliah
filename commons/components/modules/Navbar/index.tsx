6b5e6d256d6f44034dba983a10f31b8608188409
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-06-03 12:22:05 +0700

    refactor(navbar): prettier

--- commons/components/modules/Navbar/index.tsx
+++ commons/components/modules/Navbar/index.tsx
@@ -230,9 +230,12 @@ const Navbar = ({
                     <div
                         className={cn(
                             'flex items-center ml-7 gap-6 hidden lg:flex',
-                            is_subscribed && (LEARNING_PAGES.some(
-                                (page) => router.asPath === page
-                            ) || router.pathname === '/komunitas/[id]' || router.pathname === '/astronotes/[slug]') &&
+                            is_subscribed &&
+                                (LEARNING_PAGES.some(
+                                    (page) => router.asPath === page
+                                ) ||
+                                    router.pathname === '/komunitas/[id]' ||
+                                    router.pathname === '/astronotes/[slug]') &&
                                 '!hidden'
                         )}>
                         {UNAUTHENTICATED_NAVBAR_BUTTONS.map(
