dd6d8b95270be71f4c5cadc79fa4ccc040b8903c
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2024-06-06 21:39:22 +1000

    Implement revamped 404 page design

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,34 @@
 import Button from 'commons/components/elements/Button';
 import Layout from 'commons/layout';
+import Features from '../landing/components/Sections/Features';
+import { CDN_URL } from "../commons/constants";
+import Image from "next/image";
 
 const NotFound = (): JSX.Element => {
     return (
-        <Layout>
-            <div className="flex flex-col h-screen justify-center items-center gap-4">
-                <h1 className="text-5xl font-bold">404 Not Found</h1>
-                <Button href="/" variant="primary">
-                    Kembali
-                </Button>
+      <Layout isFullBlackBackground>
+        <div className="flex flex-col items-center justify-center min-h-screen pt-8 md:pt-20 gap-8">
+          <div className="flex flex-col items-center gap-4 px-4 md:px-8">
+            <h1 className="text-xl font-sans font-bold text-center">Page Not Found</h1>
+            <p className="text-base font-body text-center">Maaf, kami tidak bisa menemukan halaman yang kamu cari</p>
+          </div>
+          <div className="relative w-full max-w-96 md:max-w-3xl">
+            <div className="w-full h-0 pb-[40%]">
+              <Image
+                src={`${CDN_URL}/assets/404.png`}
+                priority
+                alt="404"
+                layout="fill"
+                objectFit="contain"
+              />
             </div>
-        </Layout>
+          </div>
+          <Features />
+          {/*<Button href="/" variant="primary" className="mt-8">*/}
+          {/*  Kembali*/}
+          {/*</Button>*/}
+        </div>
+      </Layout>
     );
 };
 
