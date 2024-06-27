950d0d1211905f46acd83ddfa53dd438fb7a0ce6
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-06-27 19:00:12 +0700

    fix(html schema): fixed no matches found error #2

--- pages/komunitas/[category]/[id].tsx
+++ pages/komunitas/[category]/[id].tsx
@@ -85,7 +85,7 @@ const DetailKomunitas = ({
                     },
                     hasPart: {
                         '@type': 'WebPageElement',
-                        cssSelector: 'answers',
+                        cssSelector: '#answers',
                         isAccessibleForFree: false
                     }
                 }}
