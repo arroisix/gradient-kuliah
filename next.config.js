31923ae660229fa620667bfcc664929253d2ccb1
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-07-02 14:32:15 +0700

    feat(adjust courses and books slug): added redirection for courses landing page and book detail page

--- next.config.js
+++ next.config.js
@@ -52,7 +52,157 @@ module.exports = {
                 destination: `https://gradient-sitemap.s3.ap-southeast-1.amazonaws.com/${env}/:file`,
                 permanent: false,
                 basePath: false
-            }
+            },
+            {
+                source: '/kelas/fisdas1',
+                destination: '/kelas/fisika-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/kimdas1',
+                destination: '/kelas/kimia-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/fisdas2',
+                destination: '/kelas/fisika-dasar-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/kimdas2',
+                destination: '/kelas/kimia-dasar-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/anum',
+                destination: '/kelas/analisis-numerik',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/ptsl',
+                destination: '/kelas/pengantar-teknik-sipil-dan-lingkungan',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/matdis',
+                destination: '/kelas/matematika-diskrit',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/ldh',
+                destination: '/kelas/logika-dan-himpunan',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/probstat',
+                destination: '/kelas/probabilitas-dan-statistika',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/kalkulus1',
+                destination: '/kelas/kalkulus-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/kelas/kalkulus2',
+                destination: '/kelas/kalkulus-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/astronotes-mekanika-fluida',
+                destination: '/perpustakaan/astronotes/rangkuman-mekanika-fluida',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/astronotes-kimia-organik',
+                destination: '/perpustakaan/astronotes/rangkuman-kimia-organik',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/astronotes-statika',
+                destination: '/perpustakaan/astronotes/rangkuman-statika',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/astronotes-persamaan-diferensial',
+                destination: '/perpustakaan/astronotes/rangkuman-persamaan-diferensial',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/fisdas1-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-fisika-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/kalkulus2-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-kalkulus-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/kimdas1-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-kimia-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/kimdas2-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-kimia-dasar-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/fisdas2-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-fisika-dasar-2',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/astronotes/kalkulus1-astronotes',
+                destination: '/perpustakaan/astronotes/rangkuman-kalkulus-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/bank-soal/bank-soal-kalkulus1',
+                destination: '/perpustakaan/bank-soal/pembahasan-soal-kalkulus-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/bank-soal/bank-soal-fisdas1',
+                destination: '/perpustakaan/bank-soal/pembahasan-soal-fisika-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/bank-soal/bank-soal-kimdas1',
+                destination: '/perpustakaan/bank-soal/pembahasan-soal-kimia-dasar-1',
+                permanent: true,
+                basePath: false
+            },
+            {
+                source: '/perpustakaan/bank-soal/bank-soal-kalkulus2',
+                destination: '/perpustakaan/bank-soal/pembahasan-soal-kalkulus-2',
+                permanent: true,
+                basePath: false
+            },
         ];
     }
 };
