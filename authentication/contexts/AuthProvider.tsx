e6790d3b5efd5a4986d3cd6e379a5e559026b571
rifqifarelmuhammad <rifqifarel222@gmail.com>
2024-07-10 21:44:51 +0700

    refactor(student data update): prettier

--- authentication/contexts/AuthProvider.tsx
+++ authentication/contexts/AuthProvider.tsx
@@ -41,7 +41,11 @@ export function AuthProvider({
     const router = useRouter();
 
     useEffect(() => {
-        if (!isProfileComplete && router.pathname !== '/onboarding' && router.pathname !== '/keluar-perangkat') {
+        if (
+            !isProfileComplete &&
+            router.pathname !== '/onboarding' &&
+            router.pathname !== '/keluar-perangkat'
+        ) {
             router.push('/onboarding');
         }
     }, [isProfileComplete, router]);
