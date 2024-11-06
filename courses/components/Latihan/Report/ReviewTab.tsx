994d8fe8d64561631c27d00cef2726866516dc6c
Syafiqo Octaviano <syafiqoarashy@gmail.com>
2024-11-06 19:02:56 +1000

    fix: Fix overflow issue on options

--- courses/components/Latihan/Report/ReviewTab.tsx
+++ courses/components/Latihan/Report/ReviewTab.tsx
@@ -158,7 +158,7 @@ const Option = ({
     console.log('option.text type:', typeof option.text);
     return (
         <div
-            className={`flex justify-between items-center p-3 my-1 rounded-md w-full max-w-[608px] h-[45px] ${
+            className={`flex flex-grow justify-between items-center p-3 my-1 rounded-md w-full max-w-[608px] min-h-[45px] ${
                 isSelected || isCorrect
                     ? isCorrect
                         ? 'bg-[#2AC27A]'
