5ef483b5ac0951b395552f936c6c07ddaaa609d4
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2024-12-13 18:02:45 +0700

    Revert "feat: Implement dashboard prompt bar"
    This reverts commit 67cd171ed01f955bffe5de4795fe37b5808c4eb6.

--- dashboard/containers/index.tsx
+++ dashboard/containers/index.tsx
@@ -4,7 +4,6 @@ import ReferralModal from 'referral/components/ReferralModal';
 import DashboardBanner from './dashboardBanner';
 import DashboardContent from './dashboardContent';
 import RenewalCard from 'payment/components/RenewalCard';
-import DashboardPromptBar from '../../copilot/components/DashboardPromptBar/DashboardPromptBar';
 
 const DashboardContainer = (): JSX.Element => {
     const router = useRouter();
@@ -17,7 +16,6 @@ const DashboardContainer = (): JSX.Element => {
 
     return (
         <section className="flex flex-col w-full gap-6 pb-4 mx-auto sm:overflow-x-clip md:overflow-x-visible max-w-screen-2xl">
-            <DashboardPromptBar />
             <RenewalCard />
             <DashboardBanner />
             <DashboardContent />
