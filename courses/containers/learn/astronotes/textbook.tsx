99f5da2c7c7aa7123e3874f1606657a873355f99
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-07-24 01:50:53 +0700

    fix: use diff field for current context on textbook and bank soal

--- courses/containers/learn/astronotes/textbook.tsx
+++ courses/containers/learn/astronotes/textbook.tsx
@@ -57,8 +57,8 @@ const TextbookSolution = ({
     const currentTextbookContext = data?.problem && getTextbookDetail?.book ? {
         id: data.problem.id,
         title: getTextbookDetail.book.title,
-        subtitle: data.problem.chapter_name || '', // TODO (Steven) : Check here
-        header: data.problem.section_name || '', // TODO (Steven) : Check here
+        subtitle: data.problem.chapter || '', 
+        header: data.problem.section || '',
         contentType: 'textbook_problem' as const
     } : undefined;
 
