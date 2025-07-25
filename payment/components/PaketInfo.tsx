e7fb15b895338d3522dd1fd4e674f8a193743f46
bimantorow <bimantoro.widyadana@ui.ac.id>
2025-07-25 18:12:23 +0700

    feat: finish packet info

--- payment/components/PaketInfo.tsx
+++ payment/components/PaketInfo.tsx
@@ -11,10 +11,9 @@ const PaketInfo = (): JSX.Element => {
     const router = useRouter();
     const { packet } = usePayment();
     const isAuthenticated = useSelector(getIsAuthenticated);
-    const { data: activePacket, isLoading } = useGetActiveSubscriptionQuery(
-        undefined,
-        { skip: !isAuthenticated }
-    );
+    const { data: activePacket } = useGetActiveSubscriptionQuery(undefined, {
+        skip: !isAuthenticated
+    });
 
     const deactivateAfter = activePacket?.deactivate_after
         ? moment(activePacket.deactivate_after)
@@ -27,30 +26,28 @@ const PaketInfo = (): JSX.Element => {
         : null;
 
     return (
-        <section className="fixed inset-x-0 z-10 flex items-center justify-between w-full px-4 py-4 top-16 md:px-12 bg-graphite-900">
-            <p className="flex items-center gap-2 font-body">
-                <span className="text-sm uppercase">
-                    {packet?.packet_name.replace('Paket', '')}
-                </span>
-                {activeUntil && (
-                    <span className="text-sm uppercase">
-                        Aktif Hingga {activeUntil.format('D MMMM YYYY')}
-                    </span>
-                )}
-                <span className="w-[1px] h-[18px] bg-neutral-600"></span>
-                <span className="text-base font-bold">
-                    Rp{Number(packet?.price).toLocaleString('id')}
-                </span>
-            </p>
+        <section className="fixed inset-x-0 top-16 z-10 bg-graphite-900 border-gray-700">
+            <div className="mx-32 flex items-center justify-between px-4 py-4">
+                <div className="flex flex-col">
+                    <h2 className="text-neutral-50 font-semibold text-xl">
+                        {packet?.packet_name}
+                    </h2>
+                    {activeUntil && (
+                        <p className="mt-1 text-neutral-400 text-sm">
+                            Aktif hingga {activeUntil.format('D MMMM YYYY')}
+                        </p>
+                    )}
+                </div>
 
-            <Link
-                href={{
-                    pathname: '/pembayaran/ubah-paket',
-                    query: router.query
-                }}
-                className="text-[#7264EB] font-sans font-bold text-sm">
-                Ubah Paket
-            </Link>
+                <Link
+                    href={{
+                        pathname: '/pembayaran/ubah-paket',
+                        query: router.query
+                    }}
+                    className="text-[#7264EB] font-semibold text-sm">
+                    Ubah Paket
+                </Link>
+            </div>
         </section>
     );
 };
