fbc17ec769e20ab9a05cab49eb759861c859580f
bimantorow <bimantoro.widyadana@ui.ac.id>
2025-08-01 16:30:16 +0700

    change to floor

--- payment/containers/AuthenticateCreditCardContainer.tsx
+++ payment/containers/AuthenticateCreditCardContainer.tsx
@@ -60,7 +60,7 @@ const AuthenticateCreditCardContainer = ({
 
         window.Xendit.card.createAuthentication(
             {
-                amount: Math.round(trx.payment_amount),
+                amount: Math.floor(trx.payment_amount),
                 token_id: card.card_token,
                 external_id: trx.id
             },
