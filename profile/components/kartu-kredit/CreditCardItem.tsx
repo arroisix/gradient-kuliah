d9d3dc4702586020028fea4b745279e41109633c
bimantorow <bimantoro.widyadana@ui.ac.id>
2025-08-04 12:50:06 +0700

    match hover colors with payment page

--- profile/components/kartu-kredit/CreditCardItem.tsx
+++ profile/components/kartu-kredit/CreditCardItem.tsx
@@ -34,7 +34,7 @@ const CreditCardItem: React.FC<CreditCardItemProps> = ({ card }) => {
     return (
         <Link
             href={`/profil/kartu-kredit/${card.id}`}
-            className="flex items-center space-x-4 p-4 hover:bg-graphite-800 transition-colors w-full text-left border-b border-graphite-600 rounded-md">
+            className="flex items-center space-x-4 p-4 hover:bg-graphite-800/50 transition-colors w-full text-left border-b border-graphite-600 rounded-md">
             <div className="w-8 h-8 shrink-0 bg-white rounded-md flex items-center justify-center">
                 {getCardLogo(card.brand)}
             </div>
