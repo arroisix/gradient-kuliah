6e0bdcea280b5049b8e86328fc58f9883eb4f361
vissutagunawan <vglim3653@gmail.com>
2025-08-16 18:42:12 +0700

    add for you tab

--- courses/hooks/useCourses.tsx
+++ courses/hooks/useCourses.tsx
@@ -1,7 +1,7 @@
 import { useGetPrivateListCoursesV2Query } from 'courses/redux/api/privateCourseV2Api';
 
 const useCourses = (
-    section: 'all' | 'newly-released' | 'coming-soon' | 'trending',
+    section: 'all' | 'newly-released' | 'coming-soon' | 'trending' | 'for-you',
     sort: 'latest' | 'popularity' | 'lexicography'
 ) => {
     const { isLoading, error, data } = useGetPrivateListCoursesV2Query(
