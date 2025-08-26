915d6b7267ac8a112928212e36d15963d15807ac
vissutagunawan <vglim3653@gmail.com>
2025-08-26 21:36:31 +0700

    add optional isTrending boolean for trending badge rendering in ProductCard

--- commons/components/elements/ProductCard/index.tsx
+++ commons/components/elements/ProductCard/index.tsx
@@ -6,26 +6,28 @@ const ProductCard = ({
     orientation,
     category,
     isReference = false,
+    isTrending = false,
     ...props
 }: ProductCardProps & { isReference?: boolean }): JSX.Element => {
     const categoryLabel: { [key: string]: string } = {
         [category]: category,
         Catatan: 'Astronotes',
         Textbook: 'Textbook Solution',
-        BankSoal: 'Bank Soal',
+        BankSoal: 'Bank Soal'
     };
 
     return orientation == 'horizontal' ? (
-        <HorizontalProductCard 
-            {...props} 
-            category={categoryLabel[category]} 
+        <HorizontalProductCard
+            {...props}
+            category={categoryLabel[category]}
             isReference={isReference}
         />
     ) : (
-        <VerticalProductCard 
-            {...props} 
-            category={categoryLabel[category]} 
+        <VerticalProductCard
+            {...props}
+            category={categoryLabel[category]}
             isReference={isReference}
+            isTrending={isTrending}
         />
     );
 };
