f0a5272bf4ae3559582db0dc51f94dc8dfd97c1f
vissutagunawan <vglim3653@gmail.com>
2025-09-08 20:49:48 +0700

    add SearchSection, remove unused imports

--- dashboard/containers/index.tsx
+++ dashboard/containers/index.tsx
@@ -1,19 +1,15 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { getIsAuthenticated } from 'authentication/redux/selectors/userSelector';
 import ReferralModal from 'referral/components/ReferralModal';
 import DashboardContent from './dashboardContent';
-import DashboardPromptBar from 'copilot/components/DashboardPromptBar/DashboardPromptBar';
 import DashboardFeatures from '../components/DashboardFeatures';
 import LanjutBelajarSection from '../components/LanjutBelajar/LanjutBelajarSection';
-import DashboardUpdatesBanner from 'dashboard/components/DashboardBanner';
+import SearchSection from 'dashboard/components/Search/SearchSection';
 
 const DashboardContainer = (): JSX.Element => {
     const router = useRouter();
     const { checkout } = router.query;
     const [isReferralModalOpen, setIsReferralModalOpen] = useState(false);
-    const isAuthenticated = useSelector(getIsAuthenticated);
 
     useEffect(() => {
         if (checkout === 'success') setIsReferralModalOpen(true);
@@ -21,8 +17,7 @@ const DashboardContainer = (): JSX.Element => {
 
     return (
         <section className="flex flex-col w-full gap-6 pb-4 mx-auto sm:overflow-x-clip md:overflow-x-visible max-w-screen-2xl">
-            <DashboardUpdatesBanner />
-            {isAuthenticated && <DashboardPromptBar />}
+            <SearchSection />
             <DashboardFeatures />
             <LanjutBelajarSection />
             <DashboardContent />
