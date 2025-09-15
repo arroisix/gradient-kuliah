785529a8fa1b2812cbaed3e4084c00c6e7266198
vissutagunawan <vglim3653@gmail.com>
2025-09-15 19:51:58 +0700

    replace extrabold to bold only

--- dashboard/components/CarouselSection.tsx
+++ dashboard/components/CarouselSection.tsx
@@ -98,7 +98,7 @@ const CarouselSection: React.FC<CarouselSectionProps> = ({
     return (
         <div className="w-full mb-4">
             <div className="flex items-center justify-between mb-4">
-                <h2 className="text-lg font-extrabold md:text-xl">{title}</h2>
+                <h2 className="text-lg font-bold md:text-xl">{title}</h2>
                 <div className="flex items-center gap-2">
                     <div className="flex gap-2">
                         <button
