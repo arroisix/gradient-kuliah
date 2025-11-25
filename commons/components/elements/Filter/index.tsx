311f5403190147c39c56d8746d45d0ea17c18982
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-11-25 11:09:06 +0700

    fix css stacking order issue on dropdown by setting z-index to zero

--- commons/components/elements/Filter/index.tsx
+++ commons/components/elements/Filter/index.tsx
@@ -57,7 +57,7 @@ const Filter = ({
             {/* Desktop: Dropdown */}
             <div
                 className={cn(
-                    'hidden md:block dropdown z-10',
+                    'hidden md:block dropdown z-0',
                     fullWidth ? 'w-full' : '',
                     className
                 )}>
