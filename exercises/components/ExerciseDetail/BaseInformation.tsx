3a066a14fa69074163787f335ba4ec86ec3636e3
rifqifarel <rifqi.farel@mekari.com>
2025-12-03 12:16:54 +0700

    update exercise base information

--- exercises/components/ExerciseDetail/BaseInformation.tsx
+++ exercises/components/ExerciseDetail/BaseInformation.tsx
@@ -61,26 +61,26 @@ const BaseInformation = () => {
                     </h3>
                 </div>
             </div>
-            <div className="flex items-center gap-3 w-full">
+            <div className="flex items-stretch gap-3 w-full">
                 {exercise?.course.name && (
-                    <span className="flex items-center gap-1 flex-col py-3 flex-1 rounded-lg bg-violet-3">
+                    <span className="flex items-center justify-center flex-col py-3 flex-1 rounded-lg bg-violet-3">
                         <GraduationCap
                             className="text-purple-6"
                             size={width < 768 ? 14 : 18}
                         />
                         <span className="text-xs md:text-sm text-purple-6 text-center">
-                            {exercise.course.name}
+                            {exercise.course.name} asda asdad asdad
                         </span>
                     </span>
                 )}
-                <span className="flex items-center gap-1 flex-col py-3 flex-1 rounded-lg bg-violet-3">
+                <span className="flex items-center justify-center flex-col py-3 flex-1 rounded-lg bg-violet-3">
                     <List color="#7D89CC" size={width < 768 ? 14 : 18} />
                     <span className="text-xs md:text-sm text-purple-6 text-center">
                         {exercise?.total_problems} Soal
                     </span>
                 </span>
                 {(exercise?.total_duration as number) > 0 && (
-                    <span className="flex items-center gap-1 flex-col py-3 flex-1 rounded-lg bg-violet-3">
+                    <span className="flex items-center justify-center flex-col py-3 flex-1 rounded-lg bg-violet-3">
                         <Clock size={width < 768 ? 14 : 18} color="#7D89CC" />
                         <span className="text-xs md:text-sm text-purple-6 text-center">
                             {(
