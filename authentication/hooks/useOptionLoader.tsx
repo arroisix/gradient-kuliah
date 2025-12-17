f2fb181e9c8108e4763140a04a18df516403dd9f
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-12-17 20:59:45 +0700

    update student recommendation to accommodate search target

--- authentication/hooks/useOptionLoader.tsx
+++ authentication/hooks/useOptionLoader.tsx
@@ -27,5 +27,32 @@ export const useOptionLoader = (
         });
     };
 
-    return { options, setOptions, loadOptions };
+    const loadTargetOptions: (
+        institution_id?: string,
+        major_id?: string
+    ) => SelectProps['loadOption'] = (institution_id, major_id) => {
+        return function (input, callback) {
+            fetchRecommendation(
+                {
+                    is_search_target: true,
+                    fieldName,
+                    input,
+                    institution_id,
+                    major_id
+                },
+                true
+            ).then((query) => {
+                const options: Option[] =
+                    query.data?.map((item) => ({
+                        value: `${item.id}:${item.name}`,
+                        label: item.name
+                    })) ?? [];
+
+                setOptions(options);
+                callback?.(options);
+            });
+        };
+    };
+
+    return { options, setOptions, loadOptions, loadTargetOptions };
 };
