b32a966cddd39de5d47a475c1a7a377fc2d53b96
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-12-30 14:05:55 +0700

    update redirection behavior

--- exercises/components/Header/ExerciseDetailHeader.tsx
+++ exercises/components/Header/ExerciseDetailHeader.tsx
@@ -43,7 +43,7 @@ const ExerciseDetailHeader = () => {
     return (
         <header className="w-full flex items-center justify-between pb-6">
             <Button
-                onClick={() => router.push(`/latihan/`)}
+                onClick={() => router.back()}
                 variant="secondary"
                 className="rounded-full text-center !p-0 !w-8 !h-8 flex items-center justify-center">
                 <ChevronLeft size={14} />
